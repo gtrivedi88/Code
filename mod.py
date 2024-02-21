dbac25c975f09c5e43e1f0e589634ff30298bb8c
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-02-21 23:44:36 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,7 @@
-success_message = f'Successfully updated the product: {form.product_name.data}'
+Successfully updated the product: <a href="/opl/product/a14b0aaa-00d9-4e37-bdad-0c145afdc0b4">3scale Backstage provider</a>
+
+# Set success message and hide the form
+            product_id=product_id
+            view_link = url_for('view_routes.view_product_details', product_id=product_id)
+            success_message = f'Successfully updated the product: <a href="{view_link}">{form.product_name.data}</a>'
+            show_form = False
