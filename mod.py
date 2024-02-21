d3bcd0588b9a2e149806b64deb01cf8d69c98900
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-02-22 00:34:51 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,7 +1,39 @@
-Successfully updated the product: <a href="/opl/product/a14b0aaa-00d9-4e37-bdad-0c145afdc0b4">3scale Backstage provider</a>
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from forms import MyForm, SearchForm, EditForm
+from datetime import date
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReferences, ProductAlias, ProductMktLife, ProductPartners, Partner, ProductComponents, ProductLog
+from datetime import datetime
+from routes.view_routes import view_routes
+from routes.add_routes import add_routes
+from routes.edit_routes import edit_routes
+import os
 
-# Set success message and hide the form
-            product_id=product_id
-            view_link = url_for('view_routes.view_product_details', product_id=product_id)
-            success_message = f'Successfully updated the product: <a href="{view_link}">{form.product_name.data}</a>'
-            show_form = False
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+
+db = SQLAlchemy()
+
+from flask_wtf import FlaskForm
+from models import ProductType
+from wtforms.validators import DataRequired, Optional, InputRequired
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField, SelectMultipleField, DateField, HiddenField 
+
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from datetime import datetime, date
+from forms import MyForm, SearchForm, EditForm
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReferences, ProductAlias, ProductMktLife, ProductPartners, Partner, ProductComponents, ProductLog
+
+
+
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from datetime import datetime, date
+from sqlalchemy import text
+from forms import MyForm, SearchForm, EditForm
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReferences, ProductAlias, ProductMktLife, ProductPartners, Partner, ProductComponents, ProductLog
+
+
+from flask import Blueprint, render_template, request
+from models import Product
+from forms import MyForm, SearchForm, EditForm
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReferences, ProductAlias, ProductMktLife, ProductPartners, Partner, ProductComponents, ProductLog
