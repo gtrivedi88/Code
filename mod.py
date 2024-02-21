f9c84bffc0e0acdea237a439bf76416b63d426bc
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-02-21 23:16:18 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,2 +1,2 @@
-<b>Product created on:</b> {{ product.created.strftime('%B %d, %Y') }} <br>
-    <b>Product last updated on:</b> {{ product.last_updated.strftime('%B %d, %Y') }}
+# Fetch the associated product logs
+    product_logs = ProductLog.query.filter_by(product_id=product_id).all()
