9e92729b2c5ebad7e54dc7dac05543891054b6e0
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-05 18:32:34 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,7 +1,1 @@
-This pipeline has a special task called verify-enterprise-contract
-
-Enterprise Contract (EC) is a special CLI that compliments Sigstore’s cosign. If you remember the build-sign-image task that produced and stored the .sig and .att artifacts into the container registry, EC evaluates that signature and attestation. EC is based on the rego policy language from Open Policy Agent and allows you to create custom rules to review cosign attest attestations. By default, EC works with the SLSA provence attestation coming out of Tekton Chains.
-
-Enterprise Contract, as part of this promotion pipeline, is verifing that the container image did in fact come from the corporate standard/approved build system. In other words, this container image was not built on some random person’s laptop, it was built in the secure build system. The .att, .sbom and .sig are stored alongside the container image and a tool like skopeo can be used to move all items together.
-
-https://enterprisecontract.dev/
+With this update, `PipelineRun` details page in the *Developer* or *Administrator* perspective of the web console provides a visual representation of identified vulnerabilities, categorized by the severity (critical, high, medium, and low) after you update your task and associated pipeline in a specific format. Additionally, it provides an option to download or view Software Bill of Materials (SBOMs) for enhanced transparency and control within your supply chain. 
