51cdce284d1d2854247d962967d19c17e8972701
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-05 18:44:28 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,7 @@
-With this update, `PipelineRun` details page in the *Developer* or *Administrator* perspective of the web console offers a *Vulnerabilities* row. This row provides a visual representation of identified vulnerabilities, categorized by the severity (critical, high, medium, and low) when you update your tasks and associated pipelines to a specific format. Additionally, it provides an option to download or view Software Bill of Materials (SBOMs) for enhanced transparency and control within your supply chain. 
+* With this update, `PipelineRun` details page, accessible from both the *Developer* or *Administrator* perspective of the web console, introduces a *Vulnerabilities* row. This new row offers a visual representation of identified vulnerabilities, categorized by the severity (critical, high, medium, and low). To enable this feature, ensure your tasks and associated pipelines are updated according to the specified format.
+
++
+[NOTE]
+====
+You can also review the vulnerabilities in the Vulnerabilities column in the pipeline run list view page.
+====
