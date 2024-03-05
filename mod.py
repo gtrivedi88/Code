5c670966544d696ec1a673347e74a3fbc797ead1
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-05 18:47:17 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,7 +1,1 @@
-* With this update, `PipelineRun` details page, accessible from both the *Developer* or *Administrator* perspective of the web console, introduces a *Vulnerabilities* row. This new row offers a visual representation of identified vulnerabilities, categorized by the severity (critical, high, medium, and low). To enable this feature, ensure your tasks and associated pipelines are updated according to the specified format.
-
-+
-[NOTE]
-====
-You can also review the vulnerabilities in the Vulnerabilities column in the pipeline run list view page.
-====
+* With this update, `PipelineRun` details page, accessible from both the *Developer* or *Administrator* perspective of the web console, introduces a *Vulnerabilities* row. This new row offers a visual representation of identified vulnerabilities, categorized by the severity (critical, high, medium, and low). To enable this feature, ensure your tasks and associated pipelines are updated according to the specified format. Additionally, one enabled, you can access the *Vulnerabilities* information in the Vulnerabilities column in the pipeline run list view page.
