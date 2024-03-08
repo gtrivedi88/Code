ef9a27c4351caa6d2ef3651e8d92782e96a9fc75
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-08 11:46:44 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,48 +1,26 @@
-This demonstration takes the audience on a journey across the Software Development Lifecycle (SDLC), from code, to build, through continuous deployment and finally to running in production. An end-to-end DevSecOps CI/CD demonstration of Red Hat Trusted Application Pipeline (RHTAP) incorporating Developer Hub, a developer self-service portal based on Backstage, to standardize and expedite developer onboarding with golden path templates imbued with security guardrails.
+== Access and elevate your organization’s security defense
+“Access and elevate” represents a two-phased approach to enhancing cybersecurity with in the software development lifecycle (SDLC). Initially, it involves evaluating existing security measures across all stages of development. Following this assessment, the strategy focuses on elevating these practices by implementing advanced security solutions and adopting a DevSecOps CI/CD solution to standardize developer onboarding, expedite processes, and embed security from the start.
 
-RHTAP = RHDH (developer self-service) + RHTAS (signature, attestation) + RHTPA (SBOM)
+Framework in RHTAP
 
-Featured products in this demonstration include:
+RHTAP one stop shop for building, testing, and deploying source code with secure CI/CD that incorporates best practices in SLSA level 3, to help you spot critical vulnerabilities.
 
-Red Hat Trusted Application Pipeline
+Artifact scanning and management
 
-Red Hat Developer Hub
+Visibility into vulnerabilities in your software lets you proactively respond and remediate potential threats before you release your applications to your customers. RHTAP leverages RHDH (a self service developer portal), RHTAS (signature and attestations), and RHTPA to (Sofware bill of materials (SBOMs):
+Scan vulnerabilities with each pull request
+generate SBOMs
+ensure that your container image meets SLSA guidelines by using our enterprise contract with 41+ rules.
 
-Red Hat Trusted Artifact Signer
+Shift-left and shift-down security practices
+"Shift-left" and "shift-down" are strategies that emphasize the early integration of security measures within the SDLC. By introducing security checks at the code entry point and embedding security practices from the moment of project creation, organizations can ensure a more secure development process from the start.
 
-Red Hat Trusted Profile Analyzer
 
-Red Hat Advanced Cluster Security
+Ready-to-use software templates
+RHTAP offers ready-to-use software templates play a pivotal role in this approach, providing pre-defined pathways that embed security practices directly into the development process. This not only streamlines workflow but also significantly lowers the cognitive load on developers, allowing them to focus more on innovation and less on security concerns.
 
-Red Hat OpenShift Pipelines (Tekton)
+[NOTE]
+You can customize the ready-to-use software templates for as per your organization’s requirements.
 
-Red Hat OpenShift GitOps (ArgoCD)
-
-Red Hat Quay
-
-Red Hat OpenShift Dev Spaces
-
-Red Hat OpenShift
-
-Within a large enterprise the time required for a newly hired app developer to become productive is often measured in weeks. In this demonstration, we are going to show you how to shrink those weeks down to minutes with a developer-optimized, self-service, Internal Development Platform (IDP) that pre-integrates software supply chain security practices from the moment of project inception.
-
-This is the story of how Red Hat imbues your software supply chain with the content, templates, signatures, attestations, and SBOMs that accelerate your custom application development across the software development lifcycle of code, build, deploy and run. This is the story of how Red Hat empowers your Platform Engineering teams who then in turn super charge your custom application developers.
-
-You might have heard the phrase "shift-left" as it relates to security practices. That phrase primarily focuses on the idea that security checks should not simply be executed manually (human-powered) and at the very end of the path-to-production pipeline but brought forward in time.
-
-In this demonstration, we are going to see the ultimate of "shift-left" by introducing security checks at the point of code entry. We are also going to see "shift-down", injection of security practices from the moment of project creation, embodied by the golden path template, and fully integrated into the platform itself.
-
-Remember the ultimate goal of a well managed platform-as-a-product (PaaP) is to serve its customers, and in the case of an IDP, that means the enterprise application development teams. The IDP can best serve its customers by dramatically lowering the developer’s cognitive load, allowing app devs to focus on the code that contributes to the next business outcome.
-
-In this story, we are are going to hear about 3 major new ideas:
-
-Software Catalog - one URL to rule them all
-
-Documentation as Code
-
-Templates with golden paths that automate securing your software supply chain
-
-No tickets, no wait, let’s go
-
-Please contact the RHTAP product team on https://redhat.enterprise.slack.com/archives/C06D1L9N6J3 [Slack] for assistance and to provide feedback related to these materials. (Note: Please open the link in a new tab)
-
+The Path Forward
+Adopting a DevSecOps mindset and uitlizing RHTAP fosters a secure, efficient development environment. This continuous journey of assessment and elevation prepares organizations to meet current and future cynbersecurity challenges head-on.
