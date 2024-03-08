5a73462628e0a6ef69555336a28ed457e15a139c
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-08 13:53:19 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,12 +1,12 @@
 = Building your application with software templates
 
-Once RHTAP is installed, cluster administrators can customize the RHDH portal, tailoring it with specific templates and enhancements. This customization enables developers to concentrate solely on coding by streamlining development processes and alleviating concerns related to pipelines, vulnerabilities, and policies.
+With the installation of {ProductShortName}, cluster administrators can customize the {RHDHLongName} portal, tailoring it with specific templates and enhancements. This customization enables developers to concentrate solely on coding by streamlining development processes and alleviating concerns related to pipelines, vulnerabilities, and policies.
 
-Our getting-started resources guide plaform engineers through the exploration of provided samples for software and pipeline templates. This exploration is crucial for understanding how RHTAP facilitates a secure supply chain before any further customization.
+However, before any customization, cluster administrators can use this guide to explore provided samples for software and pipeline templates. This exploration is crucial for understanding how RHTAP facilitates a secure supply chain before any further customization.
+
+You can use ready-to-use software templates to build and accelerate your application's path to production. These software templates are engineered to seamlessly integrate with Red Hat's extensive array of tools ({RHDHShortName}, {RHTASShortName}, and {RHTPAShortName}) and technologies, providing you with robust framework for a secure, efficient, and developer-friendly software development lifecycle (SDLC) within an on-premise environment.
 
 Emulating best practices, Red Hat enriches your software supply chain with essential components - templates, signatures, attestation, and SBOMs - to expedite custom application development through all SDLC stages: code, build, deploy, and run.
-
-Use ready-to-use software templates to build and accelerate your application's path to production. These software templates are engineered to seamlessly integrate with Red Hat's extensive array of tools ({RHDHShortName}, {RHTASShortName}, and {RHTPAShortName}) and technologies, providing you with robust framework for a secure, efficient, and developer-friendly software development lifecycle (SDLC) within an on-premise environment.
 
 With RHTAP you can build and containerize Java, Python, Node, or Go-based applications
 
