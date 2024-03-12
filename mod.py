886fddfebf690065a5ba15ff15803268ba7a4763
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-12 13:11:53 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,4 @@
-expected a string that is sequences of [a-zA-Z0-9] separated by any of [-_.], at least 1 and at most 63 characters in total
+ In the *Deployment Namespace* field, enter the namespace or cluster where your application will be deployed to.
+ In the *Deployment Namespace* field, enter the namespace or cluster where your application will be deployed to.
+it does not need to be exist any more. The app-of-apps structure can handle the namespace creation.
+but you may want to mention the field here is for namespace prefix. so the actual namespaces will be created are `<prefix>-development`, `<prefix>-stage` and `<prefix>-prod`. I will also update the UI to note that
