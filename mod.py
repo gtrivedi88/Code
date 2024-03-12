f9f35dba992dc9916395a6c54a8d6d21a42ce3fa
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-12 13:34:06 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,4 +1,3 @@
- In the *Deployment Namespace* field, enter the namespace or cluster where your application will be deployed to.
- In the *Deployment Namespace* field, enter the namespace or cluster where your application will be deployed to.
-it does not need to be exist any more. The app-of-apps structure can handle the namespace creation.
-but you may want to mention the field here is for namespace prefix. so the actual namespaces will be created are `<prefix>-development`, `<prefix>-stage` and `<prefix>-prod`. I will also update the UI to note that
+git commit --amend --author="Gaurav Trivedi gtrivedi
+@redhat.com"
+fatal: --author 'Gaurav Trivedi gtrivedi@redhat.com' is not 'Name <email>' and matches no existing author
