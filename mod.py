9ca384d8d5d9734cda794e50905e728e011cd413
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-15 11:01:58 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -5,56 +5,76 @@
 [id="deploy-application-and-view-security-insights_{context}"]
 = Deploy application and view security insights
 
-Your organization may follow its unique process from development to production. A common approach involves stages from development, to pre-production, and finally production, guided by specific rules and triggers for moving builds forward.
+Organizations typically follow a structured process for deploying applications, moving from development through pre-production to production. This process is often automated and regulated by defined rules and triggers.
 
-As an example, we use ArgoCD in OpenShift GitOps for continuous deployment across all stages. Deployment initiation requires updating the GitOps repository.
+This guide illustrates deploying applications using ArgoCD in OpenShift GitOps, facilitating continuous deployment across all stages. Deployment initiation requires updates to the GitOps repository.
 
 [NOTE]
 ====
-The procedures explained in this guide is just one way to transition applications; organizations can adapt any suitable method.
+This deployment approach is an example; organizations may adopt any method that suits their workflow.
 ====
 
 [Discrete]
-== Promoting a build to a pre-production environment
+== Promoting a build to a pre-production or production environment
 
-To promote a build to pre-production or production environment, simply make a pull or push request from your repository. This triggers a Tekton pipeline that updates the GitOps repository, enabling ArgoCD to deploy the application.
+Promoting a build to the pre-production or production environment involves updating the GitOps repository through `push` or `pull` requests in your repository, which in turn triggers a Tekton pipeline.
 
-. Open the local clone-repository that you cloned when initiating a build pipeline.
+. Open the repository that you cloned when initiating a build pipeline.
 
++
+[NOTE]
+====
 Ensure that the local clone is up-to-date
+====
 
-. You can trigger a promotion pipeline either via a push or pull request. Pull request is a preferred method.
+. Trigger the promotion pipeline through a `push` or `pull` request, with pull requests being the preferred method.
 
+.. If your application is on development environment, run the following command to promote it to staging environment. The promotion pipeline interprets the messages in these commands to execute the corresponding promotion.
+
++
+[options="nowrap"]
+----
 git commit --allow-empty -m "/promote dev"
+----
 
-promote stage ideally means promote dev to stage
+.. If your application is on stanging environment, run the following command to move it to production environment. The promotion pipeline interprets the messages in these commands to execute the corresponding promotion.
 
++
+[options="nowrap"]
+----
 git commit --allow-empty -m "/promote stage"
+----
 
-promote stage ideally means promote stage to prod
-
-What happens is the pipeline will run, it will check for the messages the commands, if it sees the command it will implement the command and will do the promotion.
-
-. git push
+. Execute `git push`.
 
 .Verification
-You can go to the CD tab, and in the ArgoCD hisory section and in different columns you can see the latest message which will give you can idea of the current status of the app (for example, appname-prod), instance, deployment details, author of the pipeline run, commit message (for example, Promote stage to prod), and resulted container image that was moved to prod
 
+. Navigate to the *CD* tab and review the ArgoCD history.
 
-* Once the release pipeline has completed, you can see your application has been promoted to pre-prod and is available via the Topology plug-in.
+.  Various columns display the latest updates, showing the application's (for example, `appname-prod`) current status , deployment details, author, commit message (for example, Promote stage to prod), and the container image promoted to production.
+
+. You can also, navigate to the *Topology* tab to view your application in various namespaces.
 
 = View security insights
 
-The promotion pipeline run offers a visual representation of all the tasks in a pipeline. A "green" status for each task indicates a successful pass through all checks, simplifying your development workflow by minimizing the need for detailed pipeline oversight.
+The promotion pipeline run offers a visual representation of all the tasks in a pipeline. A *green* status indicates successful completion, streamlining your workflow without extensive oversight.
 
-The pipeline run includes the following standard tasks for code retrieval, compilation, handle promotion action:
+The initial build pipeline tasks comprise:
 
-== Enterprise contract task in the pipeline run
+* *`clone-repository`*: Clones the specified repository into the workspace, readying it for action with the git-clone Task.
 
-Within the promotion pipeline, a task named `verify-enterpirse-contract` enhances security by validating container images. This task leverages the Enterprise Contract (EC), a command-line interface that works along side Sigstore’s cosign, to assess the integrity of image signatures and attestations.
+* *`verify-enterprise-contract`*: enhances security by validating container images ensuring they originates from a corporate standard or approved build system. This task leverages the Enterprise Contract (EC) policies, a command-line interface that works along side Sigstore’s cosign, to assess the integrity of image signatures and attestations.
 
-The `build-sign-image` task generates `.sig` (signature) and `.att` (attestation) artifacts, storing them in the container registry. EC evaluates these artifacts using the Lego policy language from the Open Policy Agent, enabling the creation of custom rules for reviewing cosign attestations. By default, EC examines the SLASA provenance attestation generated by Tekton Chains.
+* *`handle-promotion-action`*: it extracts the container images based on your commit message, for example from development environment or standing environment, and then again based on your commit message, pushes these images to the appropriate environments, for example from development environment or standing environment
 
-The primary function of EC in the promotion pipeline is to verify that the container image originates from a corporate standard or approved build system, ensuring it was not created on an unsecured environment. Alongside the container image, `.att`, `.sbom`, and `.sig` files are stored, facilitating their collective management and transfer potentially with tools like Skopeo.
+
+
+== Enterprise contract task
+
+The Enterprise Contract is a set of tools for maintaining software supply chain security, and for the definition and enforcement of policies related to how container images are built and tested.
+
+The Enterprise Contract’s purpose is to ensure container images produced by AppStudio meet certain clearly defined requirements before they are considered releasable. Should a container image not meet the requirements the Enterprise Contract will produce a list of the reasons why so they can be addressed as required to produce a releasable build.
+
+The Konflux build process uses Tekton Chains to produce a signed in-toto attestation of the build pipeline. Enterprise Contract then uses that signed attestation to cryptographically verify that the build was not tampered with, and to check the build against a set of policies. The policies attest that the build process followed a prescribed set of best practices, plus organization specific policies as required.
 
 For more information on Enterprise Contract, refer <Doc on which Jocelyn is working>.
