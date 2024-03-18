bd94f897483c2b8499d82f9aacb8daeae1c4b119
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-18 14:48:04 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,17 @@
-Are we covering automatic configuration of GitLab webhook URLs during the install process, like in the case with GitHub? or is this something a user has to add manually in GitLab during the install or after the install process.
+== Advanced Cluster Security (ACS) Tasks
+This is how we are using ACS inside RHTAP. For ACS task to be successfull here we expect that you have already installed and configured it during the install RHTAP process. If you diFor more information on ACS, refer ACS docs.
+
+If ACS is installed and configured, the status of the ACS tasks (for example, `roxctl image scan`) appears green. Otherwise, it appears greyed out.
+
+.The ACS tasks in the pipeline run
+<image>
+
+The pipeline incorporates three ACS tasks using `roxctl` to perform comprehensive security checks:
+
+* `roxctl image scan` - Returns the components and vulnerabilities found in the image in JSON format
+
+* `roxctl image check` - Checks the build-time violations of your security policies in the image. For example, 'No log4j allowed' or perhaps no curl, wget nor a package manager in a production image.
+
+* `roxctl deployment check` - Checks the build-time and deploy-time violations of your security policies in YAML deployment files.
+
+These tasks ensure adherence to security policies and configurations right from the development stage.
