697fd264384c3d9c3365c1735a715c3b41b0da63
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-18 13:46:44 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,15 +1,1 @@
-== Viewing the pipeline run
-
-To view the pipeline run after making changes to your code:
-
-. Return to {RHDHShortName} platform
-. Navigate to the *Catalog* and select the updated component.
-. Open the *CI* tab to access the Pipeline Run section.
-
-For additional information about your applicaiton, you can also review different tabs that {RHDHShortName} offers:
-
-* *Image Registry*: This tab offers every thing about the image built by the pipeline. For example, a container image with `.att`, `sig`, and `.sbom`.
-
-* *CD*: This tab offers deployements that are depoyed by ArgoCD and GitOps.
-
-* *Topology*: This tab offers view to your application deployed in the development namespace. 
+Are we covering automatic configuration of GitLab webhook URLs during the install process, like in the case with GitHub? or is this something a user has to add manually in GitLab during the install or after the install process.
