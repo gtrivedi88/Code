55c3ef84e0bb68a275ac36f2143f08122e3a3df0
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-18 13:23:50 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,15 @@
-The `rhtap-app` is the default deployment namespace prefix. As a cluster administrator can customize the default deployment namespace prefix. To customize the default deployment namespace prefix, refer <tbd>.
+== Viewing the pipeline run
+
+To view the pipeline run after making changes to your code:
+
+. Return to {RHDHShortName} platform
+. Navigate to the *Catalog* and select the updated component.
+. Open the *CI* tab to access the Pipeline Run section.
+
+For additional information about your applicaiton, you can also review different tabs that {RHDHShortName} offers:
+
+* *Image Registry*: This tab offers every thing about the image built by the pipeline. For example, a container image with `.att`, `sig`, and `.sbom`.
+
+* *CD*: This tab offers deployements that are depoyed by ArgoCD and GitOps.
+
+* *Topology*: This tab offers view to your application deployed in the development namespace. 
