71eaf6297e70e7fe3e4ce8d09437fbed4f89e575
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-22 21:25:31 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,15 +1,4 @@
-== Customizing the sample pipeline template repository to your workflow
+Yes, the gitop-repo could say "It invokes the gitops-pull-request pipeline (located in the pipelines directory)" so when you look at the description in the pipelines it says something like
+Pipelines.  Implementations of build and promotion validation pipelines used referenced from the gitops-repo and source repo event handlers.
 
-The sample pipeline template repository serves as a starting point. You can further customize it based on your organization's specific needs:
-
-On the pipeline templates repository you will find following template pipelines:
-
-* `docker-build-rhtap`: This pipeline is used to create dockerfile based sscs builds. The pipeline run by this runner will clone the source, build an image with SBOM, and attestations and push these to the users image registry.
-
-* gitops-repo: This pipeline is used to validate pull-requests into the gitops repository. This runner will call the gitops-pull-request pipeline which will validate the images being updated. Its' designed to be used for promotion flows, where the state of one environment is being promoted to the successor environment (dev to stage, or stage to prod).
-
-* pipelines:    
-
-* This pipeline is used to create dockerfile based sscs builds. The pipeline run by this runner will clone the source, build an image with SBOM, and attestations and push these to the users image registry.
-
-* tasks: add new tasks or modify existing ones to suit your requirements. For example, you can replace ACS tasks with some other checks or add new tasks into the pipeline.
+the source-repo  and gitops-repos contents get installed into users repositories, they can find what the pipelines are actually doing by looking into the pipelines directory
