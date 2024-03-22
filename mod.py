af6746884524a101fcf9d35dd55c7008e223495a
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-22 23:17:23 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,4 +1,1 @@
-Yes, the gitop-repo could say "It invokes the gitops-pull-request pipeline (located in the pipelines directory)" so when you look at the description in the pipelines it says something like
-Pipelines.  Implementations of build and promotion validation pipelines used referenced from the gitops-repo and source repo event handlers.
-
-the source-repo  and gitops-repos contents get installed into users repositories, they can find what the pipelines are actually doing by looking into the pipelines directory
+If you want to promote configuration changes in addition to the container image from the development to the stage environment, copy the changes from the `deployment-patch.yaml` file located in the `development` directory and paste them into the `deployment-patch.yaml` file in the `stage` directory.
