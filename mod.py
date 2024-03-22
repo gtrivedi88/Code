88a7578faf4af4a02ffdc9454da3d826b9e9a393
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-22 13:47:15 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,3 @@
 {ProductShortName} introduces ready-to-use software templates in {RHDHShortName} to expedite your application's journey to production. These templates are meticulously designed for seamless integration with Red Hat's comprehensive suite of tools ({RHDHShortName}, {RHTASShortName}, {RHTPAShortName}) and technologies. This integration furnishes a solid framework for a secure, effective, and developer-oriented SDLC within an on-premises environment.
+
+Furthermore, {ProductShortName} supports the development and containerization of applications in popular programming languages such as Java, Python, Node.js, and Go, broadening the scope for application creation.
