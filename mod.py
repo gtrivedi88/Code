5558e19cf2ba00850770435a49a7239b03545af0
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-22 17:37:51 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,10 +1,1 @@
-. Commit and push your updates. 
-
-. Create a pull request (PR). This pull request triggers a promotion pipeline run which validates the changed container images for {ECLong} ({ECShort}) policies. The promotion pipeline run offers a visual representation of all the tasks in a pipeline. A *green* status indicates successful completion. You can review the triggered promotion pipeline in the *CI* tab in {RHDHShortName}.
-
-. Review and merge the PR. This triggers ArgoCD to apply build promotion related change. You can navigate to the *CD* tab to review the ArgoCD history for the latest deployment updates. Various columns display the latest updates, showing the application's (for example, `appname-prod`) current status , deployment details, author, commit message (for example, Promote stage to prod), and the container image promoted to production. 
-
-
-.Verification
-
-* Navigate to the *Topology* tab to review promotion of your application across appropriate namespaces.
+You can click any on the tasks in the pipeline run to view logs and additional information that gets generated when a task successfllty run, whicj is marked by a green check. 
