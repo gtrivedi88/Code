6dff1957cc095a40de7339d6b98703440a6c3fce
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-22 14:03:09 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,18 +1,7 @@
+:_mod-docs-content-type: PROCEDURE
+
+[id="build-an-application-with-sample-software-templates_{context}"]
 = Build an application with sample software templates
-
-Upon the installation of {ProductShortName}, cluster administrators have the capability to tailor the {RHDHLongName} portal with specific templates and enhancements. This customization process is crucial for enabling developers to focus primarily on coding by simplifying development workflows and mitigating concerns related to pipelines, vulnerabilities, and policies.
-
-Before proceeding with customization, it's essential for cluster administrators to familiarize themselves with the available software and pipeline templates through this guide. Such exploration is key to grasping how {ProductShortName} supports a secure supply chain, laying the groundwork for any subsequent customization.
-
-{RHDHShortName} operates as an inclusive developer platform, facilitating the creation of developer portals. It offers engineering teams a unified platform that enhances the development process, providing an assortment of tools and resources for crafting high-quality software efficiently.
-
-The purpose of Developer Hub is to simplify the complexities of developing and maintaining developer portals by offering:
-
-* A centralized dashboard for accessing all available developer tools and resources, aimed at boosting productivity.
-
-* Self-service options within set guardrails for cloud-native application development, adhering to enterprise-grade best practices.
-
-* Enhanced security and governance for developers throughout the organization.
 
 {ProductShortName} introduces ready-to-use software templates in {RHDHShortName} to expedite your application's journey to production. These templates are meticulously designed for seamless integration with Red Hat's comprehensive suite of tools ({RHDHShortName}, {RHTASShortName}, {RHTPAShortName}) and technologies. This integration furnishes a solid framework for a secure, effective, and developer-oriented SDLC within an on-premises environment.
 
@@ -27,3 +16,7 @@ Beyond these foundational elements, {ProductShortName}’s software templates in
 * *GitOps:* Implements a GitOps strategy by maintaining your infrastructure and application configurations in Git repositories, ensuring consistent and automated deployment across all environments.
 
 Furthermore, {ProductShortName} supports the development and containerization of applications in popular programming languages such as Java, Python, Node.js, and Go, broadening the scope for application creation.
+
+Upon the installation of {ProductShortName}, cluster administrators have the capability to tailor the {RHDHLongName} portal with specific templates and enhancements. This customization process is crucial for enabling developers to focus primarily on coding by simplifying development workflows and mitigating concerns related to pipelines, vulnerabilities, and policies.
+
+Before proceeding with customization, it's essential for cluster administrators to familiarize themselves with the available software and pipeline templates through this guide. Such exploration is key to grasping how {ProductShortName} supports a secure supply chain, laying the groundwork for any subsequent customization.
