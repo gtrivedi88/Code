639ad27f47b9db826feeea0de9909219eae8fdc6
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-26 13:38:23 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,3 @@
-*For GitLab users:* Setting up webhooks and secrets is necessary for automatic pipeline execution upon code pushes. Follow these steps to set up webhooks:
+ In the Name field, enter an application name consisting of a sequence of alphanumeric characters ([a-zA-Z0-9]), allowing for separators of dashes (-), underscores (_), or periods (.) between them. The name must contain at least 1 character and no more than 63 characters in total. 
+
+Valid application name starts from lower case alphanumeric characters or "-", start with a alphanumeric charatcer, and end with an alphanumeric character, for example my-name, r abc-123.
