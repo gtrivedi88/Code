653dbb291b9cc372a46cac9a63c0a4227a915329
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-27 13:35:33 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,7 +1,12 @@
+ ✘ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo snap changes
+ID   Status  Spawn                   Ready                   Summary
+28   Done    yesterday at 17:40 IST  yesterday at 17:40 IST  Pre-download "code" for auto-refresh
+29   Done    yesterday at 23:45 IST  yesterday at 23:45 IST  Pre-download "code" for auto-refresh
+30   Done    today at 03:35 IST      today at 03:35 IST      Pre-download "code" for auto-refresh
+31   Done    today at 06:40 IST      today at 06:40 IST      Pre-download "code" for auto-refresh
+32   Done    today at 12:45 IST      today at 12:45 IST      Pre-download "code" for auto-refresh
+33   Doing   today at 13:34 IST      -                       Auto-refresh snap "code"
+
  gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo snap refresh code
-error: cannot refresh "code": snap "code" has running apps (code), pids: 8393,9367
- ✘ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo kill 8393
- gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo kill 8393
- gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo kill 9367
- gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo snap refresh code
-error: cannot refresh "code": snap "code" has running apps (code), pids: 8393,9367
+snap "code" has no updates available
+ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  
