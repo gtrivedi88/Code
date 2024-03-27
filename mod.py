62cd138e825e5f90d2b57fd61aedd2c3a4437918
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-27 13:33:30 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,4 +1,7 @@
- ✘ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo snap refresh code
-
-error: cannot refresh "code": snap "code" has running apps (code), pids:
-       4474,4476,4477,4496,4525,4586,4753,4936,4961,5004,5121,5149,5284,8393,8848,8936,9363,9367
+ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo snap refresh code
+error: cannot refresh "code": snap "code" has running apps (code), pids: 8393,9367
+ ✘ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo kill 8393
+ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo kill 8393
+ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo kill 9367
+ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo snap refresh code
+error: cannot refresh "code": snap "code" has running apps (code), pids: 8393,9367
