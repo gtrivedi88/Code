14ded4247229d7da242f70061c8e67eb24ebe7a7
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-03-27 14:31:48 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,12 +1,29 @@
- ✘ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo snap changes
-ID   Status  Spawn                   Ready                   Summary
-28   Done    yesterday at 17:40 IST  yesterday at 17:40 IST  Pre-download "code" for auto-refresh
-29   Done    yesterday at 23:45 IST  yesterday at 23:45 IST  Pre-download "code" for auto-refresh
-30   Done    today at 03:35 IST      today at 03:35 IST      Pre-download "code" for auto-refresh
-31   Done    today at 06:40 IST      today at 06:40 IST      Pre-download "code" for auto-refresh
-32   Done    today at 12:45 IST      today at 12:45 IST      Pre-download "code" for auto-refresh
-33   Doing   today at 13:34 IST      -                       Auto-refresh snap "code"
+:_mod-docs-content-type: PROCEDURE
 
- gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo snap refresh code
-snap "code" has no updates available
- gtrivedi@gtrivedi-thinkpadt14sgen1  ~  
+[id="customizing-sample-pipelines_{context}"]
+= Customizing sample pipelines
+
+Customizing sample pipelines and workflows enables organizations to align development practices with specific requirements. This section outlines the steps to personalize pipeline template and adapt pipeline configurations to your organizational needs.
+
+.Prerequisites
+
+* You have already forked and cloned both the link:https://github.com/redhat-appstudio/tssc-sample-pipelines[sample pipelines] and link:https://github.com/redhat-appstudio/tssc-sample-templates[sample software templates] repositories locally.
+
+[discrete]
+== Customizing the sample software template repository to update Pipeline as code (`pac`) URLs*
+
+.Procedure
+
+. Access your forked sample pipelines repository and copy the URL from the web address bar. For example, https://github.com/<username>/tssc-sample-pipelines.
+
+. Access the cloned sample templates repository and in terminal run the following command:
+
++
+[source,bash]
+----
+.scripts/update-tekton-definition <your-sample-pipeline-fork-url> <your-sample-pipeline-branch>
+
+# For example, .scripts/update-tekton-definition https://github.com/<username>/tssc-sample-pipelines main
+----
+
+. Review and commit the changes and push them to your repository, effectively updating the templates.
