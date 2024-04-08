914a6e169a6aa72e7268830220c328b7ec612872
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-08 17:51:30 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,15 +1,1 @@
-== Unregister or remove your application
-
-. Select Catalog and select the component that you want to remove.
-
-. Select vertical three dots and then select Unregister entity.
-
-. On the confirmation pop up, select Unregister Location. To get back this application, you can just re-register the entity in Red Hat Developer Hub.
-
-. (Optional) To remove the application from the catalog, on the confirmation pop up, select Advanced and then select Delete Entity.
-
-+
-[NOTE]
-----
-Note that this should only be done if you know that the catalog file has been deleted at, or moved from, its origin location. If that is not the case, the entity will reappear shortly as the next refresh round is performed by the catalog. However, if you remove the application, it does not remove the application from the cluster.
-----
+This document provides information to help you get started with {ProductName}. This includes a walkthrough of {ProductShortName} foundations, install, build, code, and deploy your application.
