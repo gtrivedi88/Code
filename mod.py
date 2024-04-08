01926836c533d2810548e4052564a0a2169471bd
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-08 18:16:02 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,3 @@
-This document provides information to help you get started with {ProductName}. This includes a walkthrough of {ProductShortName} foundations, install, build, code, and deploy your application.
+In the catalog goto to the app and select the menu at top right, and "unregister" it  - this unregisters the git repo for that app
+You also need to unregister the gitops app which is in "Resources" ... Evertything is still running, it does nothing to your cluster
+To remove the running stuff you can remove the app-of-apps  oc delete application your-app-name-app-of-apps -n rhtap 
