0acef441357e231099176d93fe00675046b61700
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-08 17:33:12 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,7 +1,15 @@
-In the Name field, provide an application name that begins with either a lowercase letter or a digit. Your name may incorporate lowercase letters (a-z), numbers (0-9), dashes (-), and periods (.), but it must start and end with an alphanumeric character. Examples of valid names are my-name or abc-123, and the length should range from 1 to 63 characters.
+== Unregister or remove your application
 
-begins with either a lowercase letter or a digit.
-but it must start and end with an alphanumeric character
-begins with/must start with a lowercase alphabetic character, and end with a lowercase alphanumeric character
-Your name may incorporate lowercase letters (a-z), numbers (0-9), dashes (-), and periods (.)
-dot(.) is not allowed
+. Select Catalog and select the component that you want to remove.
+
+. Select vertical three dots and then select Unregister entity.
+
+. On the confirmation pop up, select Unregister Location. To get back this application, you can just re-register the entity in Red Hat Developer Hub.
+
+. (Optional) To remove the application from the catalog, on the confirmation pop up, select Advanced and then select Delete Entity.
+
++
+[NOTE]
+----
+Note that this should only be done if you know that the catalog file has been deleted at, or moved from, its origin location. If that is not the case, the entity will reappear shortly as the next refresh round is performed by the catalog. However, if you remove the application, it does not remove the application from the cluster.
+----
