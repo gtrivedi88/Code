8e9db312dba346fbc5aaf2ce1b7d826320f11541
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-16 18:24:36 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,19 +1,35 @@
-Press CTRL+C to quit
-[2024-04-16 18:17:19,852] ERROR in app: Exception on /opl/search-to-view-products [GET]
-Traceback (most recent call last):
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 1455, in wsgi_app
-    response = self.full_dispatch_request()
-               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 869, in full_dispatch_request
-    rv = self.handle_user_exception(e)
-         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 867, in full_dispatch_request
-    rv = self.dispatch_request()
-         ^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 852, in dispatch_request
-    return self.ensure_sync(self.view_functions[rule.endpoint])(**view_args)
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 21, in view_products
-    func.coalesce(func.array_agg(ProductType.type_name), ['No Types']).label('type_names')
-                                 ^^^^^^^^^^^^^^^^^^^^^
-AttributeError: type object 'ProductType' has no attribute 'type_name'
+class ProductType(db.Model):
+    """
+    Represents different types of products.
+
+    Attributes:
+    - type_id: Unique identifier for the product type.
+    - product_type: Type of the product.
+    """
+
+    __tablename__ = 'product_types'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    type_id = db.Column(db.String, primary_key=True)
+    product_type = db.Column(db.String(255), nullable=False)
+
+    # Relationships
+    product_types_map = db.relationship('ProductTypeMap', back_populates='product_type')
+
+class ProductTypeMap(db.Model):
+    """
+    Represents a many-to-many relationship between Product and ProductType.
+
+    Attributes:
+    - product_id: Foreign key to Product.
+    - type_id: Foreign key to ProductType.
+    """
+
+    __tablename__ = 'product_types_map'
+    __table_args__ = {'schema': 'brand_opl'}
+
+    product_id = db.Column(db.String, db.ForeignKey('brand_opl.product.product_id'),primary_key=True)
+    type_id = db.Column(db.String, db.ForeignKey('brand_opl.product_types.type_id'), primary_key=True)
+
+    # Relationships
+    product_type = db.relationship('ProductType', back_populates='product_types_map')
