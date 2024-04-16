1abfa6ddc59941ffc3ce75d40b6c1639bf6056ad
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-16 16:47:02 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,3 +1,42 @@
+from flask import Blueprint, render_template, request
+from models import Product
+from forms import MyForm, SearchForm, EditForm
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReferences, ProductAlias, ProductMktLife, ProductPartners, Partner, ProductComponents, ProductLog
+
+view_routes = Blueprint('view_routes', __name__)
+
+@view_routes.route('/opl/search-to-view-products', methods=['GET', 'POST'])
+def view_products():
+    # Initialize the search form
+    form = SearchForm()
+
+    # Define the base query for products, now including an order by clause
+    products_query = Product.query.order_by(Product.product_name)
+
+    # Filter products based on form data if the form is submitted and valid
+    if form.validate_on_submit():
+        if form.product_name.data:
+            products_query = products_query.filter(Product.product_name.ilike(f"%{form.product_name.data}%"))
+
+        if form.product_status.data and form.product_status.data != 'Select':
+            products_query = products_query.filter(Product.product_status == form.product_status.data)
+
+        if form.product_type.data and form.product_type.data:
+            type_id = form.product_type.data
+            if isinstance(type_id, list):
+                type_id = type_id[0]  # Take the first item if it's a list, adjust according to your needs
+            products_query = products_query.join(ProductTypeMap).join(ProductType).filter(ProductType.type_id == type_id)
+
+    # Get the list of products based on the filtered and now sorted query
+    products = products_query.all()
+
+    # Check if a specific product is selected to display detailed information
+    selected_product_id = request.args.get('product_id')
+    selected_product = Product.query.get(selected_product_id) if selected_product_id else None
+
+    return render_template('opl/view_search.html', form=form, products=products, selected_product=selected_product)
+
+# Route to view detailed information for a specific product
 @view_routes.route('/opl/product/<string:product_id>', methods=['GET'])
 def view_product_details(product_id):
     # Retrieve the product based on the provided product_id
@@ -18,87 +57,56 @@ def view_product_details(product_id):
 
     # Fetch the actual portfolios based on the category_ids
     portfolios = ProductPortfolios.query.filter(ProductPortfolios.category_id.in_(category_ids)).all()
+    
 
+    # Get partner choices
+    partner_choices = [(partner.partner_id, partner.partner_name) for partner in Partner.query.all()]
 
-{% if portfolios %}
-        <div class="field-pair">
-            <label>Member Of</label>
-            <ul>
-                {% for portfolio in portfolios %}
-                <li>{{ portfolio.category_name }} portfolio</li>
-                {% endfor %}
-            </ul>
-        </div>
-        {% endif %}
+    # Get component choices
+    component_choices = [('', 'Select')] + [(component.product_id, component.product_name) for component in Product.query.all()]
 
+    # Get product notes
+    product_notes = ProductNotes.query.filter_by(product_id=product_id).first()
 
+    # Fetch the associated product references
+    product_references = product.product_references.all()
 
-             {% extends 'base.html' %}
+    # Fetch the associated product aliases
+    product_aliases = product.aliases.all()
 
-{% block heading %}
-<h1 class="pf-v5-c-title pf-m-4xl">View product details</h1>
-{% endblock %}
+    # Fetch the associated product release information
+    product_mkt_life = ProductMktLife.query.filter_by(product_id=product_id).first()
 
-{% block content %}
-<div class="container">
-    <!-- Section 1: Search -->
-    <fieldset class="product-search-group">
-        <legend>Search by</legend>
-        <div id="search-form" class="my-4">
-            <form method="post" action="{{ url_for('view_routes.view_products') }}" class="form">
-                {{ form.csrf_token }}
-                {{ form.hidden_tag() }}
+    # Fetch the associated product partners
+    product_partners = ProductPartners.query.filter_by(product_id=product_id).all()
 
-                <div class="form-group">
-                    <label for="product_name">Product Name:</label>
-                    {{ form.product_name(class="form-control") }}
-                </div>
+    # Fetch the associated product components
+    product_components = ProductComponents.query.filter_by(component_id=product_id).all()
 
-                <div class="form-group">
-                    <label for="product_status">Product Status:</label>
-                    {{ form.product_status(class="form-control") }}
-                </div>
+    # Additional queries to retrieve product names based on component product_id
+    component_product_names = {comp.product_id: Product.query.get(comp.product_id).product_name for comp in product_components}
 
-                <div class="form-group">
-                    <label for="product_type">Product Type</label>
-                    {{ form.product_type(class="form-control") }}
-                </div>
+    # Fetch the associated child components
+    child_components = ProductComponents.query.filter_by(product_id=product_id).all()
 
-                {{ form.submit(style="background-color: #0a63ca; color: #ffffff; font-size: 17px; padding-right: 10px;
-                border: none; border-radius: 5px; font-weight: bold; cursor: pointer;") }}
-                {{ form.reset(style="background-color: #0a63ca; color: #ffffff; font-size: 17px; padding-left: 10px;
-                border: none; border-radius: 5px; font-weight: bold; cursor: pointer;") }}
+    component_product_name = {comp.component_id: Product.query.get(comp.component_id).product_name for comp in child_components}
 
-            </form>
-        </div>
-    </fieldset>
-
-    <!-- Section 2: Search Results -->
-    {% if form.is_submitted() and form.validate() %}
-    <fieldset class="product-search-group">
-        <legend>Search Results</legend>
-        <div id="search-results" class="my-4">
-            {% if products %}
-            <ul class="list-unstyled">
-                {% for product in products %}
-                <li class="mb-4 product-item">
-                    <div class="product-info">
-                        <h3 class="product-name">
-                            <a href="{{ url_for('view_routes.view_product_details', product_id=product.product_id) }}">
-                                {{ product.product_name }}
-                            </a>
-                        </h3>
-                        <p class="product-status"><strong>Status:</strong> {{ product.product_status }}</p>
-                        <p class="product-last-updated"><strong>Last Updated:</strong> {{ product.last_updated }}</p>
-                    </div>
-                </li>
-                {% endfor %}
-            </ul>
-            {% else %}
-            <p>No results found.</p>
-            {% endif %}
-        </div>
-    </fieldset>
-    {% endif %}
-</div>
-{% endblock %}
+    # Fetch the associated product logs
+    product_logs = ProductLog.query.filter_by(product_id=product_id).order_by(ProductLog.edit_date.desc()).all()
+    
+    return render_template('opl/view.html', 
+                           product=product,
+                           product_types=product_types,
+                           portfolios=portfolios,
+                           partner_choices=partner_choices,
+                           component_choices=component_choices,
+                           product_notes=product_notes,
+                           product_references=product_references,
+                           product_aliases=product_aliases,
+                           product_mkt_life=product_mkt_life,
+                           product_partners=product_partners,
+                           product_components=product_components,
+                           child_components=child_components,
+                           component_product_names=component_product_names,
+                           component_product_name=component_product_name,
+                           product_logs=product_logs)
