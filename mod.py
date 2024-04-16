b51ee455cd6f2c6f927f38689d298be5a585e846
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-16 18:18:29 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,3 +1,9 @@
+Press CTRL+C to quit
+[2024-04-16 18:17:19,852] ERROR in app: Exception on /opl/search-to-view-products [GET]
+Traceback (most recent call last):
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 1455, in wsgi_app
+    response = self.full_dispatch_request()
+               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 869, in full_dispatch_request
     rv = self.handle_user_exception(e)
          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -7,7 +13,7 @@
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 852, in dispatch_request
     return self.ensure_sync(self.view_functions[rule.endpoint])(**view_args)
            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 52, in view_products
-    for product in products_with_portfolios
-                   ^^^^^^^^^^^^^^^^^^^^^^^^
-UnboundLocalError: cannot access local variable 'products_with_portfolios' where it is not associated with a value
+  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 21, in view_products
+    func.coalesce(func.array_agg(ProductType.type_name), ['No Types']).label('type_names')
+                                 ^^^^^^^^^^^^^^^^^^^^^
+AttributeError: type object 'ProductType' has no attribute 'type_name'
