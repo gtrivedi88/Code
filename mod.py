2742e596f87d025095af6170bc06d4bbdc3961b2
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-16 17:56:16 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,13 +1,11 @@
-  File "/usr/lib/python3.12/site-packages/jinja2/environment.py", line 1301, in render
-    self.environment.handle_exception()
+  File "/usr/lib/python3.12/site-packages/jinja2/loaders.py", line 137, in load
+    code = environment.compile(source, name, filename)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/usr/lib/python3.12/site-packages/jinja2/environment.py", line 768, in compile
+    self.handle_exception(source=source_hint)
   File "/usr/lib/python3.12/site-packages/jinja2/environment.py", line 936, in handle_exception
     raise rewrite_traceback_stack(source=source)
-  File "/home/gtrivedi/git/gitlab/opl-ui/templates/opl/view_search.html", line 1, in top-level template code
-    {% extends 'base.html' %}
-  File "/home/gtrivedi/git/gitlab/opl-ui/templates/base.html", line 10, in top-level template code
-    {% block content %}{% endblock %}
+  File "/home/gtrivedi/git/gitlab/opl-ui/templates/opl/view_search.html", line 58, in template
+    <p class="product-portfolios"><strong>Portfolios:</strong> {{ ', '.join([name for name in
     ^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/git/gitlab/opl-ui/templates/opl/view_search.html", line 58, in block 'content'
-    <p class="product-portfolios"><strong>Portfolios:</strong> {{ ', '.join(portfolio_names) }}</p>
-    ^^^^^^^^^^^^^^^^^^^^^^^^^
-TypeError: sequence item 0: expected str instance, NoneType found
+jinja2.exceptions.TemplateSyntaxError: expected token ',', got 'for'
