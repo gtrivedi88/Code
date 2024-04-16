745926411f1399e513c15a18bf95cafd863edd4c
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-16 18:05:18 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,11 +1,13 @@
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 869, in full_dispatch_request
+    rv = self.handle_user_exception(e)
+         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 867, in full_dispatch_request
     rv = self.dispatch_request()
          ^^^^^^^^^^^^^^^^^^^^^^^
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 852, in dispatch_request
     return self.ensure_sync(self.view_functions[rule.endpoint])(**view_args)
            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 48, in view_products
-    product['portfolio_names'] = [name for name in product['portfolio_names'] if name]
-                                                   ~~~~~~~^^^^^^^^^^^^^^^^^^^
-  File "lib/sqlalchemy/cyextension/resultproxy.pyx", line 48, in sqlalchemy.cyextension.resultproxy.BaseRow.__getitem__
-TypeError: tuple indices must be integers or slices, not str
+  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 52, in view_products
+    for product in products_with_portfolios
+                   ^^^^^^^^^^^^^^^^^^^^^^^^
+UnboundLocalError: cannot access local variable 'products_with_portfolios' where it is not associated with a value
