95a7d33a8d2db9080754c14a5db3389f84a35078
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-16 15:39:19 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,8 +1,13 @@
-# Fetch the associated product portfolios
-    product_portfolios = ProductPortfolioMap.query.filter_by(product_id=product_id).all()
-
-    # Extract category_ids from the portfolio mappings
-    category_ids = [portfolio.category_id for portfolio in product_portfolios]
-
-    # Fetch the actual portfolios based on the category_ids
-    portfolios = ProductPortfolios.query.filter(ProductPortfolios.category_id.in_(category_ids)).all()
+ File "/home/gtrivedi/.local/lib/python3.12/site-packages/werkzeug/serving.py", line 326, in execute
+    write(data)
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/werkzeug/serving.py", line 301, in write
+    self.wfile.write(data)
+  File "/usr/lib64/python3.12/socketserver.py", line 840, in write
+    self._sock.sendall(b)
+  File "/usr/lib64/python3.12/ssl.py", line 1211, in sendall
+    v = self.send(byte_view[count:])
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/usr/lib64/python3.12/ssl.py", line 1180, in send
+    return self._sslobj.write(data)
+           ^^^^^^^^^^^^^^^^^^^^^^^^
+ssl.SSLEOFError: EOF occurred in violation of protocol (_ssl.c:2406)
