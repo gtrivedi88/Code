a80b77c17c2dc2ff27dd3c65667298caaffacaab
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-17 18:04:36 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,17 +1,53 @@
-== How does it work?
+{% extends 'base.html' %}
 
-{ProductName} ({ProductShortName}), empowers you to streamline and secure your entire DevSecOps CI/CD process.  This journey begins within {RHDHLongName}, a self-service portal powered by Backstage. Here, you'll find {ProductName} readily available for installation and configuration. With just a few clicks, you can set up {ProductShortName} for your development team.
+{% block heading %}
+<h1 class="pf-v5-c-title pf-m-4xl">View product details</h1>
+{% endblock %}
 
-*Secure Development from the Onset*
+{% block content %}
+<div class="container">
+    <!-- Section 1: Search -->
+    <fieldset class="product-search-group">
+        <legend>Search by</legend>
+        <div id="search-form" class="my-4">
+            <form method="post" action="{{ url_for('view_routes.view_products') }}" class="form">
+                {{ form.csrf_token }}
+                {{ form.hidden_tag() }}
 
-Once {ProductShortName} is installed and configured, developers can leverage pre-built, secure templates within {RHDHLongName}. Simply choose the most appropriate ready-to-use software template for your needs, fill in the details, and create a new application. This creates a dedicated development environment that includes everything you need: a code repository (source code and GitOps repositories), technical documentation, and a continuous integration/continuous delivery (CI/CD) pipeline.
+                <div class="form-group">
+                    <label for="product_name">Product Name:</label>
+                    {{ form.product_name(class="form-control") }}
+                </div>
 
-*Automated Security Throughout the Pipeline*
+                <div class="form-group">
+                    <label for="product_status">Product Status:</label>
+                    {{ form.product_status(class="form-control") }}
+                </div>
 
-Editing the source code triggers a pipeline run within your application. This pipeline ensures every build artifact is signed and attested for authenticity. It also scans for vulnerabilities in your code and automatically generates Software Bills of Materials (SBOMs). These SBOMs detail all components, libraries, and dependencies included in the container image, providing complete transparency into your application's makeup.
+                <div class="form-group">
+                    <label for="product_type">Product Type:</label>
+                    {{ form.product_type(class="form-control") }}
+                </div>
 
-*Review, Refine, and Release*
+                {{ form.submit(style="background-color: #0a63ca; color: #ffffff; font-size: 17px; padding-right: 10px;
+                border: none; border-radius: 5px; font-weight: bold; cursor: pointer;") }}
+                {{ form.reset(style="background-color: #0a63ca; color: #ffffff; font-size: 17px; padding-left: 10px;
+                border: none; border-radius: 5px; font-weight: bold; cursor: pointer;") }}
+            </form>
+        </div>
+    </fieldset>
 
-The pipeline presents any identified vulnerabilities for your review and remediation. You can also review the SBOM to gain a deeper understanding of your application's components. Depending on your promotion workflow, you can also promote your application through development, staging, and finally to production. Each promotion triggers another pipeline run, scanning for vulnerabilities and enforcing your Enterprise Contract (EC). The EC ensures container images meet predefined requirements before release. If an image fails this check, the EC generates a report outlining any issues that need resolution.
 
-This streamlined approach with {ProductShortName} empowers developers to focus on innovation while maintaining the highest security standards throughout the development lifecycle.
+
+    class SearchForm(FlaskForm):
+    product_name = StringField('Product Name')
+    product_alias = StringField('Product Alias')
+    product_portfolio = SelectMultipleField('Product Portfolio')
+    product_status = SelectField('Product Status', choices=[('', 'Select'), ('Deprecated', 'Deprecated'), ('Upcoming', 'Upcoming'), ('Available', 'Available')])
+    submit = SubmitField('Search Product')
+    product_type = SelectMultipleField('Product Type *')
+    reset = SubmitField('Reset Form')
+
+    def __init__(self, *args, **kwargs):
+        super(SearchForm, self).__init__(*args, **kwargs)
+        self.product_type.choices = [(pt.type_id, pt.product_type) for pt in ProductType.query.all()]
