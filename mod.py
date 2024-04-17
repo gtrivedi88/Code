834041cf5fa5717a759468e7b2d88b88911ffc92
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-17 18:13:08 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,53 +1,8 @@
-{% extends 'base.html' %}
-
-{% block heading %}
-<h1 class="pf-v5-c-title pf-m-4xl">View product details</h1>
-{% endblock %}
-
-{% block content %}
-<div class="container">
-    <!-- Section 1: Search -->
-    <fieldset class="product-search-group">
-        <legend>Search by</legend>
-        <div id="search-form" class="my-4">
-            <form method="post" action="{{ url_for('view_routes.view_products') }}" class="form">
-                {{ form.csrf_token }}
-                {{ form.hidden_tag() }}
-
-                <div class="form-group">
-                    <label for="product_name">Product Name:</label>
-                    {{ form.product_name(class="form-control") }}
-                </div>
-
-                <div class="form-group">
-                    <label for="product_status">Product Status:</label>
-                    {{ form.product_status(class="form-control") }}
-                </div>
-
-                <div class="form-group">
-                    <label for="product_type">Product Type:</label>
-                    {{ form.product_type(class="form-control") }}
-                </div>
-
-                {{ form.submit(style="background-color: #0a63ca; color: #ffffff; font-size: 17px; padding-right: 10px;
-                border: none; border-radius: 5px; font-weight: bold; cursor: pointer;") }}
-                {{ form.reset(style="background-color: #0a63ca; color: #ffffff; font-size: 17px; padding-left: 10px;
-                border: none; border-radius: 5px; font-weight: bold; cursor: pointer;") }}
-            </form>
-        </div>
-    </fieldset>
-
-
-
-    class SearchForm(FlaskForm):
-    product_name = StringField('Product Name')
-    product_alias = StringField('Product Alias')
-    product_portfolio = SelectMultipleField('Product Portfolio')
-    product_status = SelectField('Product Status', choices=[('', 'Select'), ('Deprecated', 'Deprecated'), ('Upcoming', 'Upcoming'), ('Available', 'Available')])
-    submit = SubmitField('Search Product')
-    product_type = SelectMultipleField('Product Type *')
-    reset = SubmitField('Reset Form')
-
-    def __init__(self, *args, **kwargs):
-        super(SearchForm, self).__init__(*args, **kwargs)
-        self.product_type.choices = [(pt.type_id, pt.product_type) for pt in ProductType.query.all()]
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/selectable.py", line 1323, in _match_primaries
+    return self._join_condition(left, right, a_subset=left_right)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/selectable.py", line 1346, in _join_condition
+    cls._joincond_trim_constraints(
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/selectable.py", line 1491, in _joincond_trim_constraints
+    raise exc.AmbiguousForeignKeysError(
+sqlalchemy.exc.AmbiguousForeignKeysError: Can't determine join between 'Join object on Join object on Join object on Join object on Join object on product(139668729333664) and product_portfolio_map(139668729191872)(139668727399344) and product_portfolios(139668729192112)(139668727399920) and product_types_map(139668729192208)(139668727400112) and product_types(139668740978016)(139668727400064) and product_alias(139668729194080)' and 'product_types_map'; tables have more than one foreign key constraint relationship between them. Please specify the 'onclause' of this join explicitly.
