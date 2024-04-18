97bcd92d7989d2afe5ea6be9afa9dd1e64cf0a5b
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-18 20:15:05 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,2 @@
-Red Hat OpenShift is an enterprise-ready application platform with deployment and infrastructure options that support every application and environment, for development teams to build new applications and modernize existing ones.
+
+Save developer time, reduce cognitive load using customizable, validated templates with integrated safeguards to stay compliant with security requirements. Expedite onboarding of security workflows from a centralized, self-managed software catalog backed by an extensive ecosystem of plugins, modular extensions to stay focused on building and shipping code faster.
