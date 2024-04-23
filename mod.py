fe81fc44169bd96abcd84830a2e0cb3c82c72b9f
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-04-23 17:05:50 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,41 +1,72 @@
-<!-- Product Parent/Components Information -->
+@view_routes.route('/opl/product/<string:product_id>', methods=['GET'])
+def view_product_details(product_id):
+    # Retrieve the product based on the provided product_id
+    product = Product.query.get_or_404(product_id)
 
-    {% if product_components or child_components %}
-    <fieldset class="product-component-section">
-        <legend>Product taxonomy</legend>
-        <div class="relationship-info">
+    if not product:
+        # Handle the case where the product is not found
+        return render_template('opl/view.html', product=None)
 
-            <!-- Product Parent Information -->
-            {% if product_components %}
-            <div class="parent-info">
+    # Fetch the associated product types
+    product_types = ProductType.query.join(ProductTypeMap).filter_by(product_id=product_id).all()
 
-                {% for component in product_components %}
-                {% if component_product_names[component.product_id] and component.component_type %}
-                <p>
-                    {{ component.component_type | capitalize }} of <a
-                        href="{{ url_for('view_routes.view_product_details', product_id=component.product_id) }}">{{
-                        component_product_names[component.product_id] }}</a>
-                </p>
-                {% endif %}
-                {% endfor %}
-            </div>
-            {% endif %}
+    # Fetch the associated product portfolios
+    product_portfolios = ProductPortfolioMap.query.filter_by(product_id=product_id).all()
+
+    # Extract category_ids from the portfolio mappings
+    category_ids = [portfolio.category_id for portfolio in product_portfolios]
+
+    # Fetch the actual portfolios based on the category_ids
+    portfolios = ProductPortfolios.query.filter(ProductPortfolios.category_id.in_(category_ids)).all()
+
+    # Get partner choices
+    partner_choices = [(partner.partner_id, partner.partner_name) for partner in Partner.query.all()]
+
+    # Get component choices
+    component_choices = [('', 'Select')] + [(component.product_id, component.product_name) for component in Product.query.all()]
+
+    # Get product notes
+    product_notes = ProductNotes.query.filter_by(product_id=product_id).first()
+
+    # Fetch the associated product references
+    product_references = product.product_references.all()
+
+    # Fetch the associated product aliases
+    product_aliases = product.aliases.all()
+
+    # Fetch the associated product release information
+    product_mkt_life = ProductMktLife.query.filter_by(product_id=product_id).first()
+
+    # Fetch the associated product partners
+    product_partners = ProductPartners.query.filter_by(product_id=product_id).all()
+
+    # Fetch the associated product components
+    product_components = ProductComponents.query.filter_by(component_id=product_id).all()
+
+    # Additional queries to retrieve product names based on component product_id
+    component_product_names = sorted{comp.product_id: Product.query.get(comp.product_id).product_name for comp in product_components}
+
+    # Fetch the associated child components
+    child_components = ProductComponents.query.filter_by(product_id=product_id).all()
+
+    component_product_name = {comp.component_id: Product.query.get(comp.component_id).product_name for comp in child_components}
+
+    # Fetch the associated product logs
+    product_logs = ProductLog.query.filter_by(product_id=product_id).order_by(ProductLog.edit_date.desc()).all()
     
-            {% if child_components %}
-            <br>
-            <div class="child-info">
-                <span><b>Contains</b></span>
-                {% for component in child_components %}
-                {% if component_product_name[component.component_id] and component.component_type %}
-                <p>
-                    <a href="{{ url_for('view_routes.view_product_details', product_id=component.component_id) }}">{{
-                        component_product_name[component.component_id] }}</a> ({{ component.component_type }})
-                </p>
-                {% endif %}
-                {% endfor %}
-            </div>
-            {% endif %}
-    
-        </div>
-    </fieldset>
-    {% endif %}
+    return render_template('opl/view.html', 
+                           product=product,
+                           product_types=product_types,
+                           portfolios=portfolios,
+                           partner_choices=partner_choices,
+                           component_choices=component_choices,
+                           product_notes=product_notes,
+                           product_references=product_references,
+                           product_aliases=product_aliases,
+                           product_mkt_life=product_mkt_life,
+                           product_partners=product_partners,
+                           product_components=product_components,
+                           child_components=child_components,
+                           component_product_names=component_product_names,
+                           component_product_name=component_product_name,
+                           product_logs=product_logs)
