137cbf639b665edfa26f63cdc6bb73c14053c598
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-02 18:13:19 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,4 +1,10 @@
- ✘ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo dnf install containerd.io
-Last metadata expiration check: 0:00:44 ago on Thu 02 May 2024 06:02:35 PM IST.
-No match for argument: containerd.io
-Error: Unable to find a match: containerd.io
+sudo mv nerdctl /usr/local/bin
+ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  nerdctl version
+WARN[0000] failed to inspect the rootless containerd socket address  error="stat /run/user/4204305/containerd-rootless: no such file or directory"
+WARN[0000] unable to determine buildctl version: exec: "buildctl": executable file not found in $PATH 
+Client:
+ Version:	v2.0.0-beta.5
+ OS/Arch:	linux/amd64
+ Git commit:	9236b9370203b7f3274ae8d1417cc6ddcbdd4511
+ buildctl:
+  Version:
