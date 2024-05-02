311f367ad1ede131a3c4f94aad636a70f4a37bfc
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-02 17:07:04 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,44 +1,33 @@
-Bring your own Quay repository to the build pipeline
-
-By default, all pipelines push the images to the `quay.io/redhat-user-workloads` repository. It is not mandatory to use this Quay repo, so if you want to use your own Quay repo to control user permissions, you can do this by updating the `output-image` parameter in the pipelines.
-
-.Procedure
-
-. You will need a secret to push the images to your Quay repo. 
-
-.. Follow Quay's link:https://docs.quay.io/glossary/robot-accounts.html[docs] to create a robot account for your repo.
-.. Give push access to your repository to the robot account.
-.. Retrieve the `.dockercfg` from the "Docker Configuration" menu on your robot account credentials page.
-.. Follow the steps to link:proc_creating-secrets-for-your-builds.adoc[create a secret for your build] and do it as an "Image Pull Secret".
-
-[NOTE] 
-====
-You need to delete the `"auths": {` and one `}` at the end of the docker config file for the secret to work. Until this https://issues.redhat.com/browse/KFLUXBUGS-1160[bug] is resolved. 
-For example, this is how you will get the file from Quay:
-[source, json]
-----
-{
-  "auths": {
-    "quay.io": {
-      "auth": "example",
-      "email": "example"
-    }
-  }
-}
-----
-And this is how you should upload it to Konflux:
-[source, json]
-----
-{
-  "quay.io": {
-    "auth": "example",
-    "email": "example"
-  }
-}
-----
-====
-
-[start=2]
-. Go to the GitHub repo of your component.
-. In each .yaml file in the .tekton directory, under params, locate the param named output-image:
-.. Change it to the new repository. For example: `quay.io/my-new-quay-repo/my-app:on-pr-{\{revision}}` for the `pull-request` pipeline, and `quay.io/my-new-quay-repo/my-app:{\{revision}}` for the `push` pipeline. This is a best practice to know which images come from a Pull Request and which don't.
+<fieldset class="pf-v5-c-form pf-m-horizontal">
+        <legend class="pf-v5-c-title">Product status information</legend>
+        <div class="pf-v5-c-form__group">
+            <div class="pf-v5-c-check">
+                {{ form.deprecated() }}
+                <label class="pf-v5-c-check__label" for="{{ form.deprecated.id }}">
+                    {{ form.deprecated.label }}
+                </label>
+            </div>
+            <div class="pf-v5-c-check">
+                {{ form.upcoming_change() }}
+                <label class="pf-v5-c-check__label" for="{{ form.upcoming_change.id }}">
+                    {{ form.upcoming_change.label }}
+                </label>
+            </div>
+        </div>
+    
+        <div class="pf-v5-c-form__group">
+            <div class="pf-v5-c-form__group-control ">
+                <label class="pf-v5-c-form__label" for="{{ form.product_status.id }}">
+                    <span class="pf-v5-c-form__label-text">{{ form.product_status.label }}</span>
+                </label>
+                {{ form.product_status(id="status-dropdown", class="pf-v5-c-form-control") }}
+            </div>
+            <div class="pf-v5-c-form__group-control pf-m-mb-md">
+                <label class="pf-v5-c-form__label" for="{{ form.product_status_detail.id }}">
+                    <span class="pf-v5-c-form__label-text">{{
+                    form.product_status_detail.label }}</span>
+                </label>
+                {{ form.product_status_detail(id="status-details-dropdown", class="pf-v5-c-form-control") }}
+            </div>
+        </div>
+    </fieldset>
