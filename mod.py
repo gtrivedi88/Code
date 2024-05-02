7de90df95ab4357ff7dad9cf868aeca6c8ba4927
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-02 18:03:45 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,33 +1,4 @@
-<fieldset class="pf-v5-c-form pf-m-horizontal">
-        <legend class="pf-v5-c-title">Product status information</legend>
-        <div class="pf-v5-c-form__group">
-            <div class="pf-v5-c-check">
-                {{ form.deprecated() }}
-                <label class="pf-v5-c-check__label" for="{{ form.deprecated.id }}">
-                    {{ form.deprecated.label }}
-                </label>
-            </div>
-            <div class="pf-v5-c-check">
-                {{ form.upcoming_change() }}
-                <label class="pf-v5-c-check__label" for="{{ form.upcoming_change.id }}">
-                    {{ form.upcoming_change.label }}
-                </label>
-            </div>
-        </div>
-    
-        <div class="pf-v5-c-form__group">
-            <div class="pf-v5-c-form__group-control ">
-                <label class="pf-v5-c-form__label" for="{{ form.product_status.id }}">
-                    <span class="pf-v5-c-form__label-text">{{ form.product_status.label }}</span>
-                </label>
-                {{ form.product_status(id="status-dropdown", class="pf-v5-c-form-control") }}
-            </div>
-            <div class="pf-v5-c-form__group-control pf-m-mb-md">
-                <label class="pf-v5-c-form__label" for="{{ form.product_status_detail.id }}">
-                    <span class="pf-v5-c-form__label-text">{{
-                    form.product_status_detail.label }}</span>
-                </label>
-                {{ form.product_status_detail(id="status-details-dropdown", class="pf-v5-c-form-control") }}
-            </div>
-        </div>
-    </fieldset>
+ ✘ gtrivedi@gtrivedi-thinkpadt14sgen1  ~  sudo dnf install containerd.io
+Last metadata expiration check: 0:00:44 ago on Thu 02 May 2024 06:02:35 PM IST.
+No match for argument: containerd.io
+Error: Unable to find a match: containerd.io
