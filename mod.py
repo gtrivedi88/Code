041a07f0dd2d64dc907d4fbe3f48ba6feaa3d5d8
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-09 00:34:29 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,13 +1,30 @@
-./local-build.sh
-[19:58:16.082] FATAL (antora): playbook file not found at /home/gtrivedi/git/github/konflux-docs/preview.JmucT0m0dU.yml (cwd: /home/gtrivedi/git/github/konflux-docs, playbook: preview.JmucT0m0dU.yml)
-    Cause: Error
-        at buildPlaybook (/home/gtrivedi/git/github/konflux-docs/node_modules/@antora/playbook-builder/lib/build-playbook.js:37:15)
-        at Command.<anonymous> (/home/gtrivedi/git/github/konflux-docs/node_modules/@antora/cli/lib/cli.js:129:18)
-        at Command.listener [as _actionHandler] (/home/gtrivedi/git/github/konflux-docs/node_modules/commander/lib/command.js:482:17)
-        at /home/gtrivedi/git/github/konflux-docs/node_modules/commander/lib/command.js:1283:65
-        at Command._chainOrCall (/home/gtrivedi/git/github/konflux-docs/node_modules/commander/lib/command.js:1177:12)
-        at Command._parseCommand (/home/gtrivedi/git/github/konflux-docs/node_modules/commander/lib/command.js:1283:27)
-        at /home/gtrivedi/git/github/konflux-docs/node_modules/commander/lib/command.js:1081:27
-        at Command._chainOrCall (/home/gtrivedi/git/github/konflux-docs/node_modules/commander/lib/command.js:1177:12)
-        at Command._dispatchSubcommand (/home/gtrivedi/git/github/konflux-docs/node_modules/commander/lib/command.js:1077:23)
-        at Command._parseCommand (/home/gtrivedi/git/github/konflux-docs/node_modules/commander/lib/command.js:1248:19)
+Konflux
+Overview
+Konflux is a platform for building integrated software that streamlines, consolidates, and secures the development lifecycle.
+
+Goals
+Compose software that consists of multiple components, from multiple repositories.
+Provide transparency on the software supply chain, including both what makes up the software and how it was built.
+Provide a way for software teams to release to destinations under the control of their SRE or release engineering team(s).
+Rapid bootstrapping of new software projects
+Support both existing and new projects
+Provide APIs to manage your software lifecycle
+Provide a surface for partners to integrate, add value
+Provide a unified user interface across the entire process
+Architecture Goals
+Build artifacts once with enough data to determine suitability for releasing.
+Build artifacts once that can be released to multiple locations, multiple use cases.
+Specify builds and their dependencies entirely from git and not from transient state of the build system. Employ tools like renovate to manage dependency updates.
+Build semantic reproducible artifacts. Any configuration which has the potential to affect the semantic functionality of a build should be source controlled and associated with the produced artifact (via the provenance, for example).
+Be extensible. Provide opinionated build pipelines and release pipelines, but let users extend those and create their own.
+"Shift left" the decisions for releasing into PRs; you should be able to release artifacts from a PR as soon as it is merged.
+Just in time scaling: In contrast to “just in case” scaling. The system should be able to scale without capacity reserved ahead of time.
+Static stability: the overall system continues to work when a dependency is impaired.
+Enhancements to the pipelines (the extensible elements of the system) should be rolled out in such a way that individual users can control when they accept the update to their workspaces, their processes. Use policy to drive eventual compliance.
+Each subservice can fulfill its primary use cases independently, without relying on other systems’ availability. An exception to this is the tekton [pipeline service] which provides foundational APIs on which build-service, integration-service, and release-service depend.
+Each sub-service owns its data and logic.
+Communication among services and participants is always asynchronous.
+Each sub-service is owned by one team. Ownership does not mean that only one team can change the code, but the owning team has the final decision.
+Minimize shared resources among sub-services.
+Participants: onboarding new participants, the flexibility to satisfy the technology preferences of a heterogeneous set of participants. Think of this as the ability to easily create an ecosystem and the ability to support that ecosystem’s heterogeneous needs.
+Security, Privacy, and Governance: Sensitive data is protected by fine-grained access control
