eec8a03da35f7109784337609a4a43e84bdb401a
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-14 22:23:54 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,22 +1,28 @@
-<fieldset class="product-log-group">
-        <legend>Notes and changelog</legend>
-        <div id="product-logs">
-            {% for log in existing_logs %}
-            <div class="product-log-pair" data-log-id="{{ log.log_id }}">
-                <input type="hidden" name="existing_log_id" value="{{ log.log_id }}">
+$(document).ready(function () {
+    $('#add-log').click(function () {
+        const newLogHTML = `
+            <div class="product-log-pair">
                 <div class="form-field">
-                    <label for="edit_notes_{{ log.log_id }}">{{ form.edit_notes.label }}</label>
-                    <textarea name="edit_notes_{{ log.log_id }}" cols="40">{{ log.edit_notes }}</textarea>
+                    <label>Product Log</label>
+                    <textarea name="new_edit_notes" cols="40"></textarea>
                 </div>
                 <div class="form-field">
-                    <label for="edit_date_{{ log.log_id }}">{{ form.edit_date.label }}</label>
-                    <input type="date" name="edit_date_{{ log.log_id }}" value="{{ log.edit_date.strftime('%Y-%m-%d') }}">
+                    <label>Date</label>
+                    <input type="date" name="new_edit_date" value="{{ now().strftime('%Y-%m-%d') }}">
                 </div>
                 <div class="form-field buttons-row">
-                    <button type="button" class="remove-log" data-log-id="{{ log.log_id }}">Delete</button>
+                    <button type="button" class="remove-log">Remove</button>
                 </div>
             </div>
-            {% endfor %}
-        </div>
-        <button type="button" id="add-log">Add log</button>
-    </fieldset>
+        `;
+        $('#product-logs').append(newLogHTML);
+    });
+
+    $(document).on('click', '.remove-log', function () {
+        const logId = $(this).data('log-id');
+        if (logId) {
+            $('#deleted-logs').append(`<input type="hidden" name="deleted_log_ids" value="${logId}">`);
+        }
+        $(this).closest('.product-log-pair').remove();
+    });
+});
