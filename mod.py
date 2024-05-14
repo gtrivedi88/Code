d42da30fb67db4d8f350d0cbe1c0d0b16c091e0d
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-15 00:52:43 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -54,3 +54,55 @@
         </div>
     </fieldset>
     {% endif %}
+
+/* Alias table css */
+
+.alias-table {
+    display: block;
+    width: 100%;
+    max-width: 100%;
+    overflow-x: auto;
+    border-collapse: collapse;
+    white-space: wrap;
+    
+}
+
+.alias-table-header,
+.alias-table-row {
+    display: table;
+    width: 100%;
+    table-layout: fixed;
+    
+}
+
+.alias-table-cell {
+    display: table-cell;
+    padding: 8px;
+    border: 1px solid #ddd;
+    text-align: left;
+    word-wrap: break-word !important;
+    hyphens: auto;
+    
+}
+
+.alias-table-header .alias-table-cell,
+.alias-table-row .alias-table-cell:first-child {
+    font-weight: bold;
+    background-color: #f2f2f2;
+    
+}
+
+.alias-table-row:nth-child(odd) {
+    background-color: #f9f9f9;
+}
+
+.alias-table-row:hover {
+    background-color: #eaeaea;
+}
+
+.parent-columns,
+.notes-columns {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
