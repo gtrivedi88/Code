d63b74c63db2d0b84e8218f7470d13ba4abbeaf2
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-14 23:36:41 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,40 +1,29 @@
-{% if sorted_product_components or sorted_child_components %}
-    <fieldset class="product-component-section">
-        <legend>Product taxonomy</legend>
-        <div class="relationship-info">
-            <!-- Product Parent Information -->
-            {% if sorted_product_components %}
-            <div class="parent-info">
-                {% for component in sorted_product_components %}
-                {% set component_name = component_product_names[component.product_id] %}
-                {% if component_name %}
-                <p>
-                    {{ component.component_type | capitalize }} of <a
-                        href="{{ url_for('view_routes.view_product_details', product_id=component.product_id) }}">
-                        {{ component_name }}
-                    </a>
-                </p>
-                {% endif %}
-                {% endfor %}
+{% if product_references %}
+    <fieldset class="product-reference-group">
+        <legend>Product reference information</legend>
+        <div id="product-references">
+            <div class="two-column-layout">
+                <div class="column">
+                    <label>Product reference</label>
+                    {% for reference in product_references %}
+                    {% if reference.product_link %}
+                    <div class="field-pair">
+                        <span><a href="{{ reference.product_link }}" target="_blank">{{ reference.product_link }}</a></span>
+                    </div>
+                    {% endif %}
+                    {% endfor %}
+                </div>
+                <div class="column">
+                    <label>Reference description</label>
+                    {% for reference in product_references %}
+                    {% if reference.link_description %}
+                    <div class="field-pair">
+                        <span>{{ reference.link_description }}</span>
+                    </div>
+                    {% endif %}
+                    {% endfor %}
+                </div>
             </div>
-            {% endif %}
-            &nbsp;
-            <!-- Child Components -->
-            {% if sorted_child_components %}
-            <div class="child-info">
-                <span><b>Contains</b></span>
-                {% for component in sorted_child_components %}
-                {% set child_name = component_product_name[component.component_id] %}
-                {% if child_name %}
-                <p>
-                    <a href="{{ url_for('view_routes.view_product_details', product_id=component.component_id) }}">
-                        {{ child_name }}
-                    </a> ({{ component.component_type }})
-                </p>
-                {% endif %}
-                {% endfor %}
-            </div>
-            {% endif %}
         </div>
     </fieldset>
     {% endif %}
