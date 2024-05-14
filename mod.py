db7beffb34f97aea7c425262ac6c1207be79ffc0
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-15 02:03:26 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,4 +1,34 @@
-<div class="date-info">
-        <b>Product created on:</b> {{ product.created.strftime('%B %d, %Y') }} <br>
-        <b>Product last updated on:</b> {{ product.last_updated.strftime('%B %d, %Y') }}
-    </div>
+<fieldset class="pf-v5-c-form pf-m-horizontal">
+        <legend class="pf-v5-c-title">Product Reference Information</legend>
+        <div id="product-references" class="pf-v5-l-grid pf-m-gutter">
+            <div class="pf-v5-l-grid__item pf-m-6-col">
+                <label class="pf-v5-c-form__label">
+                    <span class="pf-v5-c-form__label-text">Product Reference</span>
+                </label>
+            </div>
+            <div class="pf-v5-l-grid__item pf-m-6-col">
+                <label class="pf-v5-c-form__label">
+                    <span class="pf-v5-c-form__label-text">Reference Description</span>
+                </label>
+            </div>
+    
+            {% for reference in product_references %}
+            <div class="pf-v5-l-grid__item pf-m-6-col">
+                {% if reference.product_link %}
+                <div class="pf-v5-c-form__field">
+                    <span class="pf-v5-c-form__control">
+                        <a href="{{ reference.product_link }}" target="_blank">{{ reference.product_link }}</a>
+                    </span>
+                </div>
+                {% endif %}
+            </div>
+            <div class="pf-v5-l-grid__item pf-m-6-col">
+                {% if reference.link_description %}
+                <div class="pf-v5-c-form__field">
+                    <span class="pf-v5-c-form__control">{{ reference.link_description }}</span>
+                </div>
+                {% endif %}
+            </div>
+            {% endfor %}
+        </div>
+    </fieldset>
