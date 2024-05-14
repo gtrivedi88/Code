b6ecd373f53ed8ce6f7c3ed7cdce994ea83dece7
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-14 23:53:56 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,29 +1,32 @@
-{% if product_references %}
-    <fieldset class="product-reference-group">
-        <legend>Product reference information</legend>
-        <div id="product-references">
-            <div class="two-column-layout">
-                <div class="column">
-                    <label>Product reference</label>
-                    {% for reference in product_references %}
-                    {% if reference.product_link %}
-                    <div class="field-pair">
-                        <span><a href="{{ reference.product_link }}" target="_blank">{{ reference.product_link }}</a></span>
-                    </div>
-                    {% endif %}
-                    {% endfor %}
-                </div>
-                <div class="column">
-                    <label>Reference description</label>
-                    {% for reference in product_references %}
-                    {% if reference.link_description %}
-                    <div class="field-pair">
-                        <span>{{ reference.link_description }}</span>
-                    </div>
-                    {% endif %}
-                    {% endfor %}
-                </div>
-            </div>
+{% if product.deprecated or product.upcoming_change or product.product_status or product.product_status_detail %}
+    <fieldset class="product-status-group">
+        <legend>Product status information</legend>
+        {% if product.deprecated %}
+        <div class="field-pair">
+            <label>Deprecated</label>
+            <span>{{ "Yes" if product.deprecated else "No" }}</span>
         </div>
+        {% endif %}
+
+        {% if product.upcoming_change %}
+        <div class="field-pair">
+            <label>Upcoming change</label>
+            <span>{{ "Yes" if product.upcoming_change else "No" }}</span>
+        </div>
+        {% endif %}
+
+        {% if product.product_status %}
+        <div class="field-pair">
+            <label>Status</label>
+            <span>{{ product.product_status }}</span>
+        </div>
+        {% endif %}
+
+        {% if product.product_status_detail %}
+        <div class="field-pair">
+            <label>Status details</label>
+            <span>{{ product.product_status_detail }}</span>
+        </div>
+        {% endif %}
     </fieldset>
     {% endif %}
