ce1b4a0527ad55695f4c84cfb4318590cf4f642b
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-14 17:49:03 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,11 +1,15 @@
-<fieldset class="product-partner-group">
-        <legend>Product partners information</legend>
-        <div class="form-field">
-            <label for="{{ form.partner.id }}">{{ form.partner.label }}</label>
-            <select id="{{ form.partner.id }}" name="{{ form.partner.name }}" multiple>
-                {% for value, label in form.partner.choices %}
-                <option value="{{ value }}">{{ label }}</option>
-                {% endfor %}
-            </select>
+<fieldset class="product-notes-group">
+        <legend>Notes and changelog</legend>
+        <div id="product-notes">
+            <div class="product-notes-pair">
+                <div class="form-field">
+                    <label for="edit_date">Date</label>
+                    <input type="date" id="edit_date" name="edit_date" value="{{ today }}">
+                </div>
+                <div class="form-field">
+                    <label for="edit_notes"> {{ form.edit_notes.label }} </label>
+                    <textarea id="edit_notes" name="edit_notes" cols="40"></textarea>
+                </div>
+            </div>
         </div>
     </fieldset>
