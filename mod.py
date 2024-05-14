27603d721a17f37e9db3292659d515204ef0f1da
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-15 01:47:22 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,13 +1,25 @@
-{% if product_partners %}
-    <fieldset class="product-partner-group">
-        <legend>Product partners information</legend>
-        <label>In Partnership with</label>
-        <div class="listview">
-            <oll>
-                {% for partner in product_partners | sort(attribute='partner.partner_name') %}
-                <li>{{ partner.partner.partner_name }}</li>
-                {% endfor %}
-            </oll>
+{% if product_logs %}
+    <fieldset class="product-notes-group">
+        <legend>Notes and changelog</legend>
+        <div id="product-notes" class="notes-table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Notes</th>
+                        <th>Added By</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {% for product_log in product_logs %}
+                    <tr>
+                        <td>{{ product_log.edit_date.strftime('%m-%d-%Y') }}</td>
+                        <td>{{ product_log.edit_notes }}</td>
+                        <td>{{ product_log.username }}</td>
+                    </tr>
+                    {% endfor %}
+                </tbody>
+            </table>
         </div>
     </fieldset>
     {% endif %}
