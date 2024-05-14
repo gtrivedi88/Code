e10c5367414e8a0710d71bba7c00d6ced07e1754
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-15 01:52:43 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,25 +1,2 @@
-{% if product_logs %}
-    <fieldset class="product-notes-group">
-        <legend>Notes and changelog</legend>
-        <div id="product-notes" class="notes-table">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Notes</th>
-                        <th>Added By</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {% for product_log in product_logs %}
-                    <tr>
-                        <td>{{ product_log.edit_date.strftime('%m-%d-%Y') }}</td>
-                        <td>{{ product_log.edit_notes }}</td>
-                        <td>{{ product_log.username }}</td>
-                    </tr>
-                    {% endfor %}
-                </tbody>
-            </table>
-        </div>
-    </fieldset>
-    {% endif %}
+<a href="{{ url_for('view_routes.view_products') }}" class="button-link">View more products</a> <a href="{{ url_for('edit_routes.edit_product_details', product_id=product.product_id) }}" class="button-link">Edit this
+    product</a>
