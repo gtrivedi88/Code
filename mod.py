8431ac2082f5a3d65505f1668cd50730b55cd7e7
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-16 15:29:49 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,6 @@
-The easiest way to start a new component build is to either open a new PR in the GitHub repository that targets the component's tracked branch or comment "/retest" on an existing PR.
+home/gtrivedi/.gnupg/pubring.kbx
+---------------------------------
+sec   ed25519/C9E9A2FA72B877B3 2024-05-16 [SC]
+      101E03F1A11729950A68FF48C9E9A2FA72B877B3
+uid                 [ultimate] Gaurav Trivedi <gtrivedi@redhat.com>
+ssb   cv25519/5E1304F2995D41F5 2024-05-16 [E]
