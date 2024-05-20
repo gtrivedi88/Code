a354649a6340ba06c4c39326f3b8a6b02bf5513c
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-20 19:28:23 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,3 +1,1 @@
-This is the preferred way to define multiple applications within an application category (e.g., operators) or family/product organization (e.g., Red Hat Advanced Cluster Management) as code because it allows you to make bulk configurations to your base component YAML (such as enabling multi-arch or labeling/ownership) in a single place, the component base, rather than multiple places.
-
-I am not sure that we should call out ACM here specifically. Is there another way that we can word this?
+<p class="success">{{ success_message|safe }}</p>
