5e3356273fa1683d84cd67e3c9c2e23955184019
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-05-23 00:15:07 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,10 +1,3 @@
-You can disable the default build-time tests by configuring the default parameter to `false`
-
-[source,yaml]
-----
-- description: Skip checks against built image
-  name: skip-checks
-  type: string
-  default: "false"
-----
+export_Pipeline_Repo_URL - The URL of the forked pipeline definition repository. For example,  https://github.com/redhat-appstudio/tssc-sample-pipelines
+export_Pipeline_Repo_Branch - The branch of the forked pipeline definition repository to which you want to point <suggestions?>. For example, main.
  
