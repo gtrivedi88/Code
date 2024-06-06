ba59d3ab0425bea1ac444eecd2cb5740af387459
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-06 13:25:27 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,2 +1,23 @@
-sqlalchemy.exc.NotSupportedError: (redshift_connector.error.NotSupportedError) type 25 not mapped to pg type
-[SQL: SELECT brand_opl.product.product_id AS brand_opl_product_product_id, brand_opl.product.product_name AS brand_opl_product_product_name, brand_opl.product.product_status AS brand_opl_product_product_status, brand_opl.product.last_updated AS brand_opl_product_last_updated, coalesce(array_agg(DISTINCT brand_opl.product_portfolios.category_name) FILTER (WHERE brand_opl.product_portfolios.category_name IS NOT NULL), %s) AS portfolio_names, array_agg(DISTINCT brand_opl.product_types.product_type) AS product_types
+AttributeError: 'Comparator' object has no attribute 'filter'
+
+The above exception was the direct cause of the following exception:
+
+Traceback (most recent call last):
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 1455, in wsgi_app
+    response = self.full_dispatch_request()
+               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 869, in full_dispatch_request
+    rv = self.handle_user_exception(e)
+         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 867, in full_dispatch_request
+    rv = self.dispatch_request()
+         ^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 852, in dispatch_request
+    return self.ensure_sync(self.view_functions[rule.endpoint])(**view_args)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 22, in view_products
+    func.coalesce(func.listagg(ProductPortfolios.category_name, ', ').within_group(ProductPortfolios.category_name).filter(ProductPortfolios.category_name != None), '').label('portfolio_names'),
+                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/elements.py", line 1601, in __getattr__
+    raise AttributeError(
+AttributeError: Neither 'WithinGroup' object nor 'Comparator' object has an attribute 'filter'
