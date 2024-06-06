79dbe1fef8881abdb902a5417b8e60881bfffd7b
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-06 13:38:00 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,12 +1,3 @@
-[2024-06-06 13:31:03,955] ERROR in app: Exception on /opl/search-to-view-products [GET]
-Traceback (most recent call last):
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/elements.py", line 1599, in __getattr__
-    return getattr(self.comparator, key)
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-AttributeError: 'Comparator' object has no attribute 'filter'
-
-The above exception was the direct cause of the following exception:
-
 Traceback (most recent call last):
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 1455, in wsgi_app
     response = self.full_dispatch_request()
@@ -20,9 +11,14 @@ Traceback (most recent call last):
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 852, in dispatch_request
     return self.ensure_sync(self.view_functions[rule.endpoint])(**view_args)
            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 22, in view_products
-    func.coalesce(func.listagg(ProductPortfolios.category_name, ', ').within_group(ProductPortfolios.category_name).filter(ProductPortfolios.category_name != None), '').label('portfolio_names'),
-                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/elements.py", line 1601, in __getattr__
-    raise AttributeError(
-AttributeError: Neither 'WithinGroup' object nor 'Comparator' object has an attribute 'filter'
+  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 24, in view_products
+    case(
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/_elements_constructors.py", line 850, in case
+    return Case(*whens, value=value, else_=else_)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/elements.py", line 3390, in __init__
+    new_whens: Iterable[Any] = coercions._expression_collection_was_a_list(
+                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/coercions.py", line 152, in _expression_collection_was_a_list
+    raise exc.ArgumentError(
+sqlalchemy.exc.ArgumentError: The "whens" argument to case(), when referring to a sequence of items, is now passed as a series of positional elements, rather than as a list. 
