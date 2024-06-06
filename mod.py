38d46461aee64308170e54a6a8b165aa64128e28
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-06 23:02:29 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,25 +1,16 @@
-[2024-06-06 13:51:12,279] ERROR in app: Exception on /opl/search-to-view-products [GET]
-Traceback (most recent call last):
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 1455, in wsgi_app
-    response = self.full_dispatch_request()
-               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 869, in full_dispatch_request
-    rv = self.handle_user_exception(e)
-         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 867, in full_dispatch_request
-    rv = self.dispatch_request()
-         ^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/app.py", line 852, in dispatch_request
-    return self.ensure_sync(self.view_functions[rule.endpoint])(**view_args)
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 25, in view_products
-    case(
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/_elements_constructors.py", line 850, in case
-    return Case(*whens, value=value, else_=else_)
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/elements.py", line 3390, in __init__
-    new_whens: Iterable[Any] = coercions._expression_collection_was_a_list(
-                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  File "/home/gtrivedi/.local/lib/python3.12/site-packages/sqlalchemy/sql/coercions.py", line 152, in _expression_collection_was_a_list
-    raise exc.ArgumentError(
-sqlalchemy.exc.ArgumentError: The "whens" argument to case(), when referring to a sequence of items, is now passed as a series of positional elements, rather than as a list. 
+Discover
+Release Notes for Red Hat Trusted Application Pipeline 1.0
+
+Get Started
+Getting Started with Red Hat Trusted Application Pipeline, a framework designed to revolutionize cybersecurity practices across the software development lifecycle (SDLC).
+
+
+Install
+Learn how to install Red Hat Trusted Application Pipeline in your cluster
+
+
+Secure Your Software Supply Chain
+Learn how to scan your SBOM to gain actionable information about your security posture
+Customizing default software templates and build pipeline configurations
+Learn about Red Hat Trusted Application Pipeline (RHTAP) and the different features that RHTAP offers
+
