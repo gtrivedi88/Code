5d5a43b81bd20c79dea24790d141e0a260dca649
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-06 23:18:55 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,16 +1,1 @@
-Discover
-Release Notes for Red Hat Trusted Application Pipeline 1.0
-
-Get Started
-Getting Started with Red Hat Trusted Application Pipeline, a framework designed to revolutionize cybersecurity practices across the software development lifecycle (SDLC).
-
-
-Install
-Learn how to install Red Hat Trusted Application Pipeline in your cluster
-
-
-Secure Your Software Supply Chain
-Learn how to scan your SBOM to gain actionable information about your security posture
-Customizing default software templates and build pipeline configurations
-Learn about Red Hat Trusted Application Pipeline (RHTAP) and the different features that RHTAP offers
-
+Explore ready-to-use software templates for building applications that are infused with secure supply chain features, such as signatures, attestations, Software Bill of Materials (SBOM), SLSA verification, CVE scanning, and release policy guardrails.
