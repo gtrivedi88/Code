aac047d281b09fae01e4efdb593d0040fc6d3fdd
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-14 20:16:45 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,1 +1,6 @@
-This doc covers this use case: By default, a user in RHDH will see no data in the Vulnerability column and Output icons. To see the data either a user has to subscribe to RHTAP or have to manually do some settings. This doc talks about those manual settings. 
+So here is a Joke (sort of)
+After all the hard work, it seems that we no longer need to update the RHDH docs. Reason - The update was need to cover this use case: By default, a user in RHDH will see no data in the Vulnerability column and Output icons. To see the data, the user has to either subscribe to RHTAP or manually configure some settings. This doc talked about those manual settings.
+
+Now RHDH dev team considers it a BUG. They no longer want to show the empty Vulnerabilities column. A must subscribe to RHTAP.
+
+Getting confirmation on this question: How would a user know that they have to subscribe to RHTAP from RHDH docs?
