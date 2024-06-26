36ee5d2017868d3f2b77b44e575307d78beda499
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-26 14:51:02 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,36 +1,20 @@
-import React from 'react';
-import { SearchInput } from '@patternfly/react-core';
-
-export const SearchInputWithSubmitButton: React.FunctionComponent = () => {
-  const [value, setValue] = React.useState('');
-
-  return (
-    <SearchInput
-      placeholder="Find by name"
-      value={value}
-      onChange={(_event, value) => setValue(value)}
-      onSearch={(_event, value) => setValue(value)}
-      onClear={() => setValue('')}
-    />
-  );
-};
-
-
-display: flex;
-width: 469px;
-min-height: 36px;
-max-height: 36px;
-padding: 6px 8px;
-align-items: center;
-gap: 4px;
-
-
-background: var(--Secondary---pf-v5-global--secondary-color--100, #6A6E73);
-
-/* Form/Form toggles default */
-box-shadow: 0px -1px 0px 0px #8A8D90 inset, 1px 0px 0px 0px #F0F0F0 inset, 0px 1px 0px 0px #F0F0F0 inset, -1px 0px 0px 0px #F0F0F0 inset;
-
-
-Secondary/--pf-v5-global--secondary-color--100
-Text/--pf-v5-global--Color--light-100
-Icon Colors/Dark/--pf-v5-global--icon--Color--dark--light
+<div class="pf-c-search-input">
+  <div class="pf-c-input-group">
+    <div class="pf-c-search-input__bar">
+      <span class="pf-c-search-input__text">
+        <span class="pf-c-search-input__icon">
+          <i class="fas fa-search fa-fw" aria-hidden="true"></i>
+        </span>
+        <input
+          class="pf-c-search-input__text-input"
+          type="text"
+          placeholder="Find by name"
+          aria-label="Find by name"
+        />
+      </span>
+    </div>
+    <button class="pf-c-button pf-m-control" type="submit" aria-label="Search">
+      <i class="fas fa-arrow-right" aria-hidden="true"></i>
+    </button>
+  </div>
+</div>
