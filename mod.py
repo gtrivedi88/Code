8496673400e42e6a0986890ce3282effa48ae2f1
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-26 12:32:42 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -14,3 +14,23 @@ export const SearchInputWithSubmitButton: React.FunctionComponent = () => {
     />
   );
 };
+
+
+display: flex;
+width: 469px;
+min-height: 36px;
+max-height: 36px;
+padding: 6px 8px;
+align-items: center;
+gap: 4px;
+
+
+background: var(--Secondary---pf-v5-global--secondary-color--100, #6A6E73);
+
+/* Form/Form toggles default */
+box-shadow: 0px -1px 0px 0px #8A8D90 inset, 1px 0px 0px 0px #F0F0F0 inset, 0px 1px 0px 0px #F0F0F0 inset, -1px 0px 0px 0px #F0F0F0 inset;
+
+
+Secondary/--pf-v5-global--secondary-color--100
+Text/--pf-v5-global--Color--light-100
+Icon Colors/Dark/--pf-v5-global--icon--Color--dark--light
