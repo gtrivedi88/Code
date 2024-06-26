635d68dc638afac774c4b7d825a4bc84fb9e092a
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-27 00:09:33 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,13 +1,5 @@
-display: inline-flex;
-height: 29px;
-align-items: center;
-gap: -8px;
-flex-shrink: 0;
-
-border-radius: var(--Radius---pf-v5-global-BorderRadius--lg, 18px);
-border: 1px solid var(--Blue---pf-v5-global--palette--blue-100, #BEE1F4);
-background: var(--Blue---pf-v5-global--palette--blue-50, #E7F1FA);
-
-Blue/--pf-v5-global--palette--blue-600
-                Blue/--pf-v5-global--palette--blue-100
-                Blue/--pf-v5-global--palette--blue-50
+<span class="pf-v5-c-label pf-m-blue">
+  <button class="pf-v5-c-label__content" type="button">
+    <span class="pf-v5-c-label__text">Blue clickable</span>
+  </button>
+</span>
