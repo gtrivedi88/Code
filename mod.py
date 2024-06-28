ee0be867af8cf3ad15bc3decebd6b62d3b684d18
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-06-29 03:00:56 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,37 +1,23 @@
-WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
- * Running on https://127.0.0.1:5000
-Press CTRL+C to quit
-Route accessed with method: GET
-GET request received
-Existing References (SQL Query): [('2f787c28-f9c6-4fe2-97a6-ebd9d0194567', 'https://portfoliohub.redhat.com/v3/serviceskit/automate_rhel_patching_compliance', 'Red Hat Consulting: Automate RHEL Patching & Compliance on Red Hat Services Portfolio Hub')]
-Reference Forms: [<forms.EditForm object at 0x7f3692831250>]
-Fetching existing ProductMktLife data
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /opl/edit-product/2f787c28-f9c6-4fe2-97a6-ebd9d0194567 HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/css/patternfly-addons.css HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/css/patternfly.css HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/css/main.css HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/images/red-hat-logo.svg HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /assets/images/img_avatar-light.svg HTTP/1.1" 404 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/scripts/search.js HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/scripts/status.js HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/scripts/preferences_edit.js HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/scripts/alias_edit.js HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/scripts/component_edit.js HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/css/assets/fonts/RedHatText/RedHatText-Regular.woff2 HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/scripts/product_note.js HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/scripts/product_type.js HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/scripts/toggle.js HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/css/assets/fonts/RedHatDisplay/RedHatDisplay-Medium.woff2 HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/css/assets/fonts/RedHatText/RedHatText-Medium.woff2 HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:49:30] "GET /static/css/assets/fonts/RedHatDisplay/RedHatDisplay-Bold.woff2 HTTP/1.1" 200 -
-Route accessed with method: POST
-POST request received
-Form data: ImmutableMultiDict([('csrf_token', 'IjI4NzMwMjMwMGQ5ZDFhNzIxMTMyOTg1Yjk2MTk4MjJhNGQ2MzIwYmYi.Zn8o2g.uo0bFRfXWSQ58mjiTxBxamtSdLc'), ('product_name', 'Red Hat Consulting: Automate RHEL Patching & Compliance'), ('product_type', '0dd4bdc1-e3f7-476c-8e93-3ad1d3969688'), ('product_type', '4b153335-e267-403a-861a-575b94b38c0a'), ('product_description', 'An engagement that helps customers effectively manage, automate, and monitor the deployment of Red Hat Enterprise Linux updates, Common Vulnerabilities and Exposures (CVEs), and OpenSCAP compliance profiles across large, complex IT environments.'), ('product_portfolio', '4337a1ce-e04c-4881-8893-5bab1464bd6c'), ('product_note', ''), ('product_link', 'https://portfoliohub.redhat.com/v3/serviceskit/automate_rhel_patching_compliance'), ('link_description', 'Red Hat Consulting: Automate RHEL Patching & Compliance on Red Hat Services Portfolio Hub'), ('upcoming_change', 'y'), ('product_status', 'Available'), ('product_status_detail', 'General availability'), ('alias_name_8e13f416-1e5b-4054-a037-5e57859ce9a2', 'Automate RHEL Patching & Compliance'), ('alias_notes_8e13f416-1e5b-4054-a037-5e57859ce9a2', 'None         '), ('alias_type_8e13f416-1e5b-4054-a037-5e57859ce9a2', 'Short'), ('alias_approved_8e13f416-1e5b-4054-a037-5e57859ce9a2', 'on'), ('alias_name_cef572e2-e1a8-4f39-80a5-4aed4840275a', 'RHC: ARHELPC'), ('alias_notes_cef572e2-e1a8-4f39-80a5-4aed4840275a', 'None         '), ('alias_type_cef572e2-e1a8-4f39-80a5-4aed4840275a', 'Short'), ('alias_name_PLACEHOLDER', ''), ('alias_notes_PLACEHOLDER', ' '), ('alias_type_PLACEHOLDER', 'Short'), ('product_release', '2024-06-06'), ('product_release_detail', ''), ('product_release_link', ''), ('product_eol', '2024-06-11'), ('product_eol_detail', ''), ('product_eol_link', ''), ('product_id[]', ''), ('component_type[]', ''), ('existing_log_id', 'c5009115-29d7-46a7-968b-fc68fba2a963'), ('edit_notes_c5009115-29d7-46a7-968b-fc68fba2a963', 'Added; planned retirement sometime in next 6 months.'), ('edit_date_c5009115-29d7-46a7-968b-fc68fba2a963', '2023-07-28'), ('submit', 'Save changes')])
-Existing References (SQL Query): [('2f787c28-f9c6-4fe2-97a6-ebd9d0194567', 'https://portfoliohub.redhat.com/v3/serviceskit/automate_rhel_patching_compliance', 'Red Hat Consulting: Automate RHEL Patching & Compliance on Red Hat Services Portfolio Hub')]
-Reference Forms: [<forms.EditForm object at 0x7f36922cc290>]
-Fetching existing ProductMktLife data
-New Aliases Data: {}
-Deleting existing ProductMktLife data
-Updated ProductMktLife data: {'product_release': datetime.date(2024, 6, 6), 'product_release_detail': 'asdasd', 'product_release_link': 'asdasd', 'product_eol': datetime.date(2024, 6, 11), 'product_eol_detail': 'asdasd', 'product_eol_link': 'asdasd'}
-127.0.0.1 - - [29/Jun/2024 02:50:37] "POST /opl/edit-product/2f787c28-f9c6-4fe2-97a6-ebd9d0194567 HTTP/1.1" 200 -
-127.0.0.1 - - [29/Jun/2024 02:50:37] "GET /static/css/patternfly-addons.css HTTP/1.1" 200 -
+            # Delete existing references for the product
+            if existing_references:
+                ProductReferences.query.filter_by(product_id=product.product_id).delete()
+
+            # Add new references
+            product_references_data = []
+            for i in range(len(request.form.getlist('product_link'))):
+                product_link = request.form.getlist('product_link')[i]
+                link_description = request.form.getlist('link_description')[i]
+
+                if product_link and link_description:
+                    product_references_data.append({
+                        'product_link': product_link,
+                        'link_description': link_description
+                    })
+
+            for reference_data in product_references_data:
+                product_references = ProductReferences(
+                    product_id=product.product_id,
+                    product_link=reference_data['product_link'],
+                    link_description=reference_data['link_description']
+                )
+                db.session.add(product_references)
