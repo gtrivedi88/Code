86f306c3a56ede82dee5e34f27c7f503dc71576f
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-07-02 01:32:05 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -6,6 +6,8 @@ Error: While importing 'app', an ImportError was raised:
 Traceback (most recent call last):
   File "/home/gtrivedi/.local/lib/python3.12/site-packages/flask/cli.py", line 219, in locate_app
     __import__(module_name)
-  File "/home/gtrivedi/git/gitlab/opl-ui/app.py", line 13, in <module>
-    from flask_caching import Cache
-ModuleNotFoundError: No module named 'flask_caching'
+  File "/home/gtrivedi/git/gitlab/opl-ui/app.py", line 9, in <module>
+    from routes.view_routes import view_routes
+  File "/home/gtrivedi/git/gitlab/opl-ui/routes/view_routes.py", line 5, in <module>
+    from app import cache
+ImportError: cannot import name 'cache' from partially initialized module 'app' (most likely due to a circular import) (/home/gtrivedi/git/gitlab/opl-ui/app.py)
