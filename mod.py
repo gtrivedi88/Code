6359c6e4181efb897df39caee2c05d71da2e243f
Gaurav Trivedi <90042568+gtrivedi88@users.noreply.github.com>
2024-07-02 01:06:56 +0530

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,77 +1,151 @@
-# Route to view detailed information for a specific product
-@view_routes.route('/opl/product/<string:product_id>', methods=['GET'])
-def view_product_details(product_id):
-    # Retrieve the product based on the provided product_id
-    product = Product.query.get_or_404(product_id)
+from flask import Flask, render_template, flash, session, request, redirect, url_for, g, current_app
+from flask_sqlalchemy import SQLAlchemy
+import flask_saml
+import flask_principal
+from forms import MyForm, SearchForm, EditForm
+from datetime import date
+from models import db, Product, ProductType, ProductTypeMap, ProductPortfolios, ProductPortfolioMap, ProductNotes, ProductReferences, ProductAlias, ProductMktLife, ProductPartners, Partner, ProductComponents, ProductLog
+from datetime import datetime
+from routes.view_routes import view_routes
+from routes.add_routes import add_routes
+from routes.edit_routes import edit_routes
+from user_details import user_details
+import os
 
-    if not product:
-        # Handle the case where the product is not found
-        return render_template('opl/view.html', product=None)
+def determine_greeting():
+    current_hour = datetime.now().hour
+    if 5 <= current_hour < 12:
+        return "Good Morning"
+    elif 12 <= current_hour < 18:
+        return "Good Afternoon"
+    else:
+        return "Good Evening"
 
-    # Fetch the associated product types
-    product_types = ProductType.query.join(ProductTypeMap).filter_by(product_id=product_id).all()
+def create_app(test_config=None):
+    app = Flask(__name__)
 
-    # Fetch the associated product portfolios
-    product_portfolios = ProductPortfolioMap.query.filter_by(product_id=product_id).all()
+    principals = flask_principal.Principal(app)
 
-    # Extract category_ids from the portfolio mappings
-    category_ids = [portfolio.category_id for portfolio in product_portfolios]
+    app.config['SECRET_KEY'] = 'Dev'
+    app.config['SAML_METADATA_URL'] = os.environ["SAML_METADATA_URL"]
+    app.config['SAML_DEFAULT_REDIRECT'] = '/'
 
-    # Fetch the actual portfolios based on the category_ids
-    portfolios = ProductPortfolios.query.filter(ProductPortfolios.category_id.in_(category_ids)).all()
+    # Config settings taken from environment variables
+    db_driver = os.environ["DBDRIVER"]
+    pg_user = os.environ["PGUSER"]
+    pg_pass = os.environ["PGPASS"]
+    pg_host = os.environ["PGHOST"]
+    pg_port = os.environ["PGPORT"]
+    pg_db = os.environ["PGDB"]
+    app.config['SQLALCHEMY_DATABASE_URI'] = "%s://%s:%s@%s:%s/%s" % (db_driver, pg_user, pg_pass, pg_host, pg_port, pg_db)
+    if "REDSHIFT_SSL" in os.environ and os.environ["REDSHIFT_SSL"] == "True":
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'connect_args': {'ssl': True, 'sslmode': 'verify_ca'}}
 
-    # Get partner choices
-    partner_choices = [(partner.partner_id, partner.partner_name) for partner in Partner.query.all()]
+    # Initialize the database
+    db.init_app(app)
 
-    # Get component choices
-    component_choices = [('', 'Select')] + [(component.product_id, component.product_name) for component in Product.query.all()]
+    # Removing the db.create_all because:
+    # a) we already have a pre-existing db
+    # b) this is causing an error when connecting to the real db
+    #
+    # with app.app_context():
+    #    db.create_all()
 
-    # Get product notes
-    product_notes = ProductNotes.query.filter_by(product_id=product_id).first()
+    flask_saml.FlaskSAML(app)
 
-    # Fetch the associated product references
-    product_references = product.product_references.all()
+    # Actions to perform when a user logs in
+    @flask_saml.saml_authenticated.connect_via(app)
+    def on_saml_authenticated(sender, subject, attributes, auth):
+        # We have a logged in user, inform Flask-Principal
+        flask_principal.identity_changed.send(
+            current_app._get_current_object(), identity=get_identity())
 
-    # Fetch the associated product aliases
-    product_aliases = product.aliases.all()
+    # Actions to perform when a user logs out
+    @flask_saml.saml_log_out.connect_via(app)
+    def on_saml_logout(sender):
+        # Let Flask-Principal know the user is gone
+        flask_principal.identity_changed.send(
+            current_app._get_current_object(), identity=get_identity())
 
-    # Fetch the associated product release information
-    product_mkt_life = ProductMktLife.query.filter_by(product_id=product_id).first()
+    # Set the user identity for the application
+    @principals.identity_loader
+    def get_identity():
+        if 'saml' in session:
+            return flask_principal.Identity(session['saml']['subject'])
+        else:
+            return flask_principal.AnonymousIdentity()
 
-    # Fetch the associated product partners
-    product_partners = ProductPartners.query.filter_by(product_id=product_id).all()
+    # Actions to perform after setting the user identity
+    @flask_principal.identity_loaded.connect_via(app)
+    def on_identity_loaded(sender, identity):
+        # Define the permission need for all users
+        identity.provides.add(
+            flask_principal.TypeNeed("all")
+        )
+        if not isinstance(identity, flask_principal.AnonymousIdentity):
+            # Define the permission need for the user's roles
+            if "Role" in session['saml']['attributes'].keys():
+                for role in session['saml']['attributes']['Role']:
+                    identity.provides.add(
+                        flask_principal.RoleNeed(role)
+                    )
+            # Define the permission need for the user's groups
+            if "group" in session['saml']['attributes'].keys():
+                for group in session['saml']['attributes']['group']:
+                    identity.provides.add(
+                        flask_principal.Need('group', group)
+                    )
+            # Define the permission need for an authenticated user
+            identity.provides.add(
+                flask_principal.TypeNeed("authenticated")
+            )
+        else:
+            # Define the permission need for an anonymous user
+            identity.provides.add(
+                flask_principal.TypeNeed("anonymous")
+            )
 
-    # Retrieve the parent and child components
-    product_components = ProductComponents.query.filter_by(component_id=product_id).all()
-    child_components = ProductComponents.query.filter_by(product_id=product_id).all()
+    # Set the variable for the username, which we use in our masthead for an
+    # authenticated user.
+    @app.context_processor
+    def get_current_user():
+        needs={}
+        for need in g.identity.provides:
+            if need.method not in needs:
+                needs[need.method]=[]
+            needs[need.method].append(need.value)
+        return dict(user=g.identity.id, needs=needs)
 
-    # Create dictionaries to store component names for sorting
-    component_product_names = {comp.product_id: Product.query.get(comp.product_id).product_name for comp in product_components}
-    component_product_name = {comp.component_id: Product.query.get(comp.component_id).product_name for comp in child_components}
+    # If a user does not have access to a particular resource, send them
+    # to our 403 page.
+    @app.errorhandler(flask_principal.PermissionDenied)
+    def handle_permission_denied(error):
+        return render_template('403.html'), 403
 
-    # Sort the components by the names stored in the dictionaries, case-insensitive
-    sorted_product_components = sorted(product_components, key=lambda x: component_product_names.get(x.product_id, "").lower())
-    sorted_child_components = sorted(child_components, key=lambda x: component_product_name.get(x.component_id, "").lower())
+    # handle 404 errors
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html'), 404
 
+    @app.route("/")
+    def home():
+        greeting = determine_greeting()
+        if 'saml' in session and 'firstName' in session['saml']['attributes']:
+            first_name = session['saml']['attributes']['firstName'][0]
+        else:
+            first_name = "anonymous user"
+        return render_template("index.html", greeting=greeting, first_name=first_name)
 
-    # Fetch the associated product logs
-    product_logs = ProductLog.query.filter_by(product_id=product_id).order_by(ProductLog.edit_date.desc()).all()
+    # The route definition to view a product.
+    app.register_blueprint(view_routes)
 
-    return render_template('opl/view.html',
-                           product=product,
-                           product_types=product_types,
-                           sorted_product_components=sorted_product_components,
-                           sorted_child_components=sorted_child_components,
-                           portfolios=portfolios,
-                           partner_choices=partner_choices,
-                           component_choices=component_choices,
-                           product_notes=product_notes,
-                           product_references=product_references,
-                           product_aliases=product_aliases,
-                           product_mkt_life=product_mkt_life,
-                           product_partners=product_partners,
-                           product_components=product_components,
-                           child_components=child_components,
-                           component_product_names=component_product_names,
-                           component_product_name=component_product_name,
-                           product_logs=product_logs)
+    # The route definition to add a product.
+    app.register_blueprint(add_routes)
+
+    # The route definition to edit a product.
+    app.register_blueprint(edit_routes)
+
+    app.register_blueprint(user_details)
+
+    # Run the application.
+    return app
